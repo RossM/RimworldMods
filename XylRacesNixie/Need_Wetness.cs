1a43f94d2a38203df4af8ad224aab083e692c3e0
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-14 05:47:25 -0700

    Hook up wetness need/thoughts.

--- XylRacesNixie/Need_Wetness.cs
+++ XylRacesNixie/Need_Wetness.cs
@@ -15,19 +15,24 @@ namespace XylRacesNixie
         public override float CurInstantLevel {
             get
             {
-                if (lastInstantWetnessCheckTick != Find.TickManager.TicksGame)
+                if (lastInstantWetnessCheckTick == Find.TickManager.TicksGame) return lastInstantWetness;
+                lastInstantWetnessCheckTick = Find.TickManager.TicksGame;
+
+                if (!this.pawn.Spawned)
+                    lastInstantWetness = 0.0f;
+                else
                 {
                     TerrainDef terrain = this.pawn.Position.GetTerrain(this.pawn.Map);
                     WeatherDef curWeatherLerped = this.pawn.Map.weatherManager.CurWeatherLerped;
                     if (terrain.traversedThought?.defName == "SoakingWet")
                         lastInstantWetness = 1.0f;
-                    else if (curWeatherLerped.weatherThought?.defName == "SoakingWet")
+                    else if (curWeatherLerped.weatherThought?.defName == "SoakingWet" &&
+                             !this.pawn.Position.Roofed(this.pawn.Map))
                         lastInstantWetness = 1.0f;
                     else
-                        lastInstantWetness = 0;
-
-                    lastInstantWetnessCheckTick = Find.TickManager.TicksGame;
+                        lastInstantWetness = 0.0f;
                 }
+
 
                 return lastInstantWetness;
             }
