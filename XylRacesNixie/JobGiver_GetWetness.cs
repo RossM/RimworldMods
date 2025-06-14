ab3c571bda430229013ccffd8d5cc1cba5c88330
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-14 16:59:50 -0700

    Update JobGiver_GetWetness.cs

--- XylRacesNixie/JobGiver_GetWetness.cs
+++ XylRacesNixie/JobGiver_GetWetness.cs
@@ -10,66 +10,7 @@ namespace XylRacesNixie
         {
             Log.Message("Called JobGiver_GetWetness.TryGiveJob()");
 
-            var need_wetness = pawn.needs?.TryGetNeed<Need_Wetness>();
-            if (need_wetness == null)
-                return null;
-            if (need_wetness.CurLevel > 0.99f)
-                return null;
-
-            bool CellValidator(IntVec3 vec)
-            {
-                Map map = pawn.Map;
-                if (PawnUtility.KnownDangerAt(vec, map, pawn))
-                    return false;
-                if (vec.Fogged(map))
-                    return false;
-                if (vec.IsForbidden(pawn))
-                    return false;
-
-                TerrainDef terrainDef = vec.GetTerrain(map);
-                if (terrainDef.traversedThought?.defName != "SoakingWet")
-                    return false;
-                if (!vec.Standable(map))
-                    return false;
-
-                return true;
-            }
-
-            if (RCellFinder.TryFindRandomCellNearWith(pawn.Position, CellValidator, pawn.Map, out IntVec3 result, 5, 25))
-            {
-                // TODO: GoSwimming seems to be an Odyssey job
-                JobDef swim = JobDefOf.GoSwimming;
-                Job job = JobMaker.MakeJob(swim, result);
-                if (job != null)
-                {
-                    job.locomotionUrgency = LocomotionUrgency.Walk;
-                    return job;
-                }
-            }
-
-            //bool RegionValidator(Region region)
-            //{
-            //    if (!region.Room.PsychologicallyOutdoors)
-            //        return false;
-            //    if (region.IsForbiddenEntirely(pawn))
-            //        return false;
-            //    if (!region.TryFindRandomCellInRegionUnforbidden(pawn, CellValidator, out IntVec3 root))
-            //        return false;
-
-            //    return true;
-            //}
-
-            //if (CellFinder.TryFindClosestRegionWith(pawn.GetRegion(RegionType.Set_Passable), TraverseParms.For(pawn),
-            //        RegionValidator, 100, out Region result, RegionType.Set_Passable))
-            //{
-            //    if (result.TryFindRandomCellInRegionUnforbidden(pawn, CellValidator, out IntVec3 root))
-            //    {
-            //        JobDef swim = DefDatabase<JobDef>.GetNamed("Swim");
-            //        Job job = JobMaker.MakeJob(swim, root);
-            //        job.locomotionUrgency = LocomotionUrgency.Walk;
-            //        return job;
-            //    }
-            //}
+            // TODO: Hook up JoyGiver_GoSwimming when Odyssey comes out
 
             return null;
         }
