7a8e68ab2d4e2a62ac77d6344c028f3f5d53d16f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-14 05:20:39 -0700

    Code initial checkin

---
+++ XylRacesNixie/Need_Wetness.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using Verse;
+
+namespace XylRacesNixie
+{
+    public class Need_Wetness : Need_Seeker
+    {
+        private int lastInstantWetnessCheckTick;
+        private float lastInstantWetness;
+
+        public Need_Wetness(Pawn pawn) : base(pawn)
+        {
+        }
+
+        public override float CurInstantLevel {
+            get
+            {
+                if (lastInstantWetnessCheckTick != Find.TickManager.TicksGame)
+                {
+                    TerrainDef terrain = this.pawn.Position.GetTerrain(this.pawn.Map);
+                    WeatherDef curWeatherLerped = this.pawn.Map.weatherManager.CurWeatherLerped;
+                    if (terrain.traversedThought?.defName == "SoakingWet")
+                        lastInstantWetness = 1.0f;
+                    else if (curWeatherLerped.weatherThought?.defName == "SoakingWet")
+                        lastInstantWetness = 1.0f;
+                    else
+                        lastInstantWetness = 0;
+
+                    lastInstantWetnessCheckTick = Find.TickManager.TicksGame;
+                }
+
+                return lastInstantWetness;
+            }
+        }
+
+        public WetnessCategory CurCategory
+        {
+            get
+            {
+                return CurLevel switch
+                {
+                    >= 0.90f => WetnessCategory.Wet,
+                    >= 0.67f => WetnessCategory.Neutral,
+                    >= 0.34f => WetnessCategory.Dry,
+                    >= 0.01f => WetnessCategory.VeryDry,
+                    _ => WetnessCategory.Parched
+                };
+            }
+        }
+    }
+}
---
+++ XylRacesNixie/ThoughtWorker_NeedWetness.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace XylRacesNixie
+{
+    public class ThoughtWorker_NeedWetness : ThoughtWorker
+    {
+        protected override ThoughtState CurrentStateInternal(Pawn p)
+        {
+            return p.needs.TryGetNeed<Need_Wetness>()?.CurCategory switch
+            {
+                WetnessCategory.Parched => ThoughtState.ActiveAtStage(0),
+                WetnessCategory.VeryDry => ThoughtState.ActiveAtStage(1),
+                WetnessCategory.Dry => ThoughtState.ActiveAtStage(2),
+                WetnessCategory.Wet => ThoughtState.ActiveAtStage(3),
+                _ => ThoughtState.Inactive,
+            };
+        }
+    }
+}
