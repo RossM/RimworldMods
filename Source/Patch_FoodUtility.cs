57aaed40b84fd4ce0497025e0295ca3ddfc0b4a8
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-15 14:26:25 -0700

    Add Fungivore gene

--- Source/Patch_FoodUtility.cs
+++ Source/Patch_FoodUtility.cs
@@ -10,7 +10,12 @@ namespace XylRacesCore
         [HarmonyPostfix]
         static void Postfix(Pawn eater, Thing food, ref float __result)
         {
-
+            StatDef rawFungusNutritionFactor =
+                DefDatabase<StatDef>.GetNamed("RawFungusNutritionFactor", errorOnFail: false);
+            if (rawFungusNutritionFactor != null && (food.def.ingestible.foodType & FoodTypeFlags.Fungus) == FoodTypeFlags.Fungus)
+            {
+                __result *= eater.GetStatValue(rawFungusNutritionFactor);
+            }
         }
     }
 }
