ea60bde299cea4ff30e8f850fc1b9187ac628ea6
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-15 04:39:12 -0700

    Add functionality to hide buildings if certain genes aren't present

---
+++ Source/Patch_Designator_Build.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(Designator_Build), nameof(Designator_Build.Visible), MethodType.Getter)]
+    public class Patch_Designator_Build
+    {
+        [HarmonyPostfix]
+        static void Postfix(Designator_Build __instance, ref bool __result)
+        {
+            if (DebugSettings.godMode)
+                return;
+
+            BuildableDef def = __instance.PlacingDef;
+            ModExtension_BuildableDef extension = def.GetModExtension<ModExtension_BuildableDef>();
+            if (extension == null) 
+                return;
+
+            if (extension.genePrerequisites != null)
+            {
+                foreach (var gene in extension.genePrerequisites)
+                {
+                    if (!__instance.Map.mapPawns.PawnsInFaction(Faction.OfPlayer)
+                            .Any((Pawn p) => p.genes?.HasActiveGene(gene) ?? false))
+                    {
+                        __result = false;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Source/Patch_FoodUtility.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(FoodUtility), nameof(FoodUtility.NutritionForEater))]
+    public class Patch_FoodUtility_NutritionForEater
+    {
+        [HarmonyPostfix]
+        static void Postfix(Pawn eater, Thing food, ref float __result)
+        {
+
+        }
+    }
+}
