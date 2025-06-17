e758a4cc44fbbb2f813fdb79a4876c522139d73f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-17 12:57:52 -0700

    Add feral rage ability

---
+++ Source/HediffComp_ReplaceOnRemoval.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class HediffComp_ReplaceOnRemoval : HediffComp_ReplaceHediff
+    {
+        [Unsaved(false)] 
+        private bool hasTriggered = false;
+
+        public override void CompPostPostRemoved()
+        {
+            base.CompPostPostRemoved();
+            if (hasTriggered) return;
+            hasTriggered = true;
+            Trigger();
+        }
+    }
+}
