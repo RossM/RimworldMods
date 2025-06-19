d26bffcba6e17057b226c658ff4771008eb87d8e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-19 02:40:36 -0700

    Implement Titan

---
+++ Source/Patch_Pawn.cs
@@ -0,0 +1,51 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(Pawn))]
+    public class Patch_Pawn
+    {
+        [HarmonyPostfix, HarmonyPatch(nameof(Pawn.BodySize), MethodType.Getter)]
+        static void BodySize_Postfix(Pawn __instance, ref float __result)
+        {
+            List<Gene> genesGenesListForReading = __instance.genes?.GenesListForReading;
+            if (genesGenesListForReading == null)
+                return;
+
+            foreach (var gene in genesGenesListForReading)
+            {
+                var extension = gene.def.GetModExtension<ModExtension_GeneDef_Pawn>();
+                if (extension != null)
+                {
+                    __result *= extension.bodySizeFactor;
+                }
+            }
+
+        }
+
+        [HarmonyPostfix, HarmonyPatch(nameof(Pawn.HealthScale), MethodType.Getter)]
+        static void HealthScale_Postfix(Pawn __instance, ref float __result)
+        {
+            List<Gene> genesGenesListForReading = __instance.genes?.GenesListForReading;
+            if (genesGenesListForReading == null)
+                return;
+
+            foreach (var gene in genesGenesListForReading)
+            {
+                var extension = gene.def.GetModExtension<ModExtension_GeneDef_Pawn>();
+                if (extension != null)
+                {
+                    __result *= extension.healthScaleFactor;
+                }
+            }
+
+        }
+    }
+}
---
+++ Source/Patch_PawnRenderNodeWorker.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace XylRacesCore
+{
+    // Not sure if this is a good thing to modify, seems performance-sensitive
+
+    [HarmonyPatch(typeof(PawnRenderNodeWorker), nameof(PawnRenderNodeWorker.ScaleFor))]
+    public class Patch_PawnRenderNodeWorker
+    {
+        [HarmonyPostfix]
+        static void Postfix(PawnRenderNode node, PawnDrawParms parms, ref UnityEngine.Vector3 __result)
+        {
+            List<Gene> genesGenesListForReading = parms.pawn?.genes?.GenesListForReading;
+            if (genesGenesListForReading == null)
+                return;
+
+            foreach (var gene in genesGenesListForReading)
+            {
+                var extension = gene.def.GetModExtension<ModExtension_GeneDef_Rendering>();
+                if (extension != null)
+                {
+                    __result *= extension.scale;
+                }
+            }
+        }
+    }
+}
