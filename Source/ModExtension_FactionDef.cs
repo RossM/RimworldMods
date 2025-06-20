fc6a7b2336f099f2f1340319af0f6622a6218c63
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-19 20:02:11 -0700

    Add factions

---
+++ Source/ModExtension_FactionDef.cs
@@ -0,0 +1,18 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld.Planet;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class ModExtension_FactionDef : DefModExtension
+    {
+        public List<BiomeDef> allowedBiomes;
+        public List<Hilliness> allowedHilliness;
+        public bool waterRequired = false;
+    }
+}
---
+++ Source/Patch_TileFinder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld.Planet;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(RimWorld.Planet.TileFinder), nameof(RimWorld.Planet.TileFinder.RandomSettlementTileFor),
+        [typeof(PlanetLayer), typeof(Faction), typeof(bool), typeof(Predicate<PlanetTile>)])]
+    public class Patch_TileFinder
+    {
+        [HarmonyPrefix]
+        static void Prefix(PlanetLayer layer, Faction faction, bool mustBeAutoChoosable, ref Predicate<PlanetTile> extraValidator)
+        {
+            var extension = faction?.def?.GetModExtension<ModExtension_FactionDef>();
+            if (extension == null)
+                return;
+
+            if (extension.waterRequired)
+            {
+                var oldValidator = extraValidator;
+                extraValidator = planetTile =>
+                {
+                    if (oldValidator != null && !oldValidator(planetTile))
+                        return false;
+                    if (planetTile.Tile is not SurfaceTile surfaceTile) 
+                        return false;
+                    //Log.Message(string.Format("surfaceTile: {0}", surfaceTile));
+                    return surfaceTile.IsCoastal || surfaceTile.Rivers is { Count: > 0 };
+                };
+            }
+
+            if (extension.allowedBiomes != null)
+            {
+                var oldValidator = extraValidator;
+                extraValidator = planetTile =>
+                {
+                    if (oldValidator != null && !oldValidator(planetTile))
+                        return false;
+                    if (planetTile.Tile is not SurfaceTile surfaceTile) 
+                        return false;
+                    return surfaceTile.Biomes.Any(biomeDef => extension.allowedBiomes.Contains(biomeDef));
+                };
+            }
+
+            if (extension.allowedHilliness != null)
+            {
+                var oldValidator = extraValidator;
+                extraValidator = planetTile =>
+                {
+                    if (oldValidator != null && !oldValidator(planetTile))
+                        return false;
+                    if (planetTile.Tile is not SurfaceTile surfaceTile)
+                        return false;
+                    return extension.allowedHilliness.Contains(surfaceTile.hilliness);
+                };
+            }
+        }
+    }
+}
