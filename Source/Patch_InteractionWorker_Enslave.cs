907423f2a5e024181da3b9e6eaabfbf8ba3d722e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-25 04:06:32 -0700

    Start implementation on Bossaps

---
+++ Source/Patch_InteractionWorker_Enslave.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(InteractionWorker_EnslaveAttempt))]
+    static class Patch_InteractionWorker_EnslaveAttempt
+    {
+        static StatDef StatDefOfWillFallRate()
+        {
+            return DefDatabase<StatDef>.GetNamed("WillFallRate");
+        }
+
+        private static readonly InstructionMatcher ApplyWillFallRate = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 0,
+                    Pattern =
+                    [
+                        // statValue2 = initiator.GetStatValue(StatDefOf.NegotiationAbility);
+                        CodeInstruction.LoadArgument(1),
+                        new CodeInstruction(OpCodes.Ldsfld, typeof(StatDefOf).GetField(nameof(StatDefOf.NegotiationAbility))),
+                        new CodeInstruction(OpCodes.Ldc_I4_1),
+                        new CodeInstruction(OpCodes.Ldc_I4_M1),
+                        CodeInstruction.Call(typeof(StatExtension), nameof(StatExtension.GetStatValue)), 
+                    ],
+                    Output =
+                    [
+                        // statValue2 *= recipient.GetStatValue(StatDefOfWillFallRate());
+                        CodeInstruction.LoadArgument(2),
+                        CodeInstruction.Call(typeof(Patch_InteractionWorker_EnslaveAttempt), nameof(Patch_InteractionWorker_EnslaveAttempt.StatDefOfWillFallRate)),
+                        new CodeInstruction(OpCodes.Ldc_I4_1),
+                        new CodeInstruction(OpCodes.Ldc_I4_M1),
+                        CodeInstruction.Call(typeof(StatExtension), nameof(StatExtension.GetStatValue)),
+                        new CodeInstruction(OpCodes.Mul),
+                    ]
+                }
+            }
+        };
+
+        [HarmonyTranspiler, HarmonyPatch(nameof(InteractionWorker_EnslaveAttempt.Interacted))]
+        static IEnumerable<CodeInstruction> Interacted_Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            if (!ApplyWillFallRate.MatchAndReplace(ref instructionsList, out string reason, generator))
+                Log.Error(string.Format("XylRacesCore.Patch_StatWorker.GetOffsetsAndFactorsExplanation_Transpiler: {0}", reason));
+            return instructionsList;
+        }
+    }
+}
---
+++ Source/Patch_InteractionWorker_RecruitAttempt.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(InteractionWorker_RecruitAttempt))]
+    static class Patch_InteractionWorker_RecruitAttempt
+    {
+        static StatDef StatDefOfResistanceFallRate()
+        {
+            return DefDatabase<StatDef>.GetNamed("ResistanceFallRate");
+        }
+
+        private static readonly InstructionMatcher ApplyResistanceFallRate = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 0,
+                    Pattern =
+                    [
+                        // statValue2 = initiator.GetStatValue(StatDefOf.NegotiationAbility);
+                        CodeInstruction.LoadArgument(1),
+                        new CodeInstruction(OpCodes.Ldsfld, typeof(StatDefOf).GetField(nameof(StatDefOf.NegotiationAbility))),
+                        new CodeInstruction(OpCodes.Ldc_I4_1),
+                        new CodeInstruction(OpCodes.Ldc_I4_M1),
+                        CodeInstruction.Call(typeof(StatExtension), nameof(StatExtension.GetStatValue)), 
+                    ],
+                    Output =
+                    [
+                        // statValue2 *= recipient.GetStatValue(StatDefOfResistanceFallRate());
+                        CodeInstruction.LoadArgument(2),
+                        CodeInstruction.Call(typeof(Patch_InteractionWorker_RecruitAttempt), nameof(Patch_InteractionWorker_RecruitAttempt.StatDefOfResistanceFallRate)),
+                        new CodeInstruction(OpCodes.Ldc_I4_1),
+                        new CodeInstruction(OpCodes.Ldc_I4_M1),
+                        CodeInstruction.Call(typeof(StatExtension), nameof(StatExtension.GetStatValue)),
+                        new CodeInstruction(OpCodes.Mul),
+                    ]
+                }
+            }
+        };
+
+        [HarmonyTranspiler, HarmonyPatch(nameof(InteractionWorker_RecruitAttempt.Interacted))]
+        static IEnumerable<CodeInstruction> Interacted_Transpiler(IEnumerable<CodeInstruction> instructions,
+            ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            if (!ApplyResistanceFallRate.MatchAndReplace(ref instructionsList, out string reason, generator))
+                Log.Error(string.Format("XylRacesCore.Patch_StatWorker.GetOffsetsAndFactorsExplanation_Transpiler: {0}", reason));
+            return instructionsList;
+        }
+    }
+}
