820171cf4ae6e2814c349c10f083731d0c66c66f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-24 23:26:09 -0700

    Implement bio-rejection

---
+++ Source/Hediff_BioRejection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class Hediff_BioRejection : Hediff
+    {
+        public override float Severity
+        {
+            get => pawn.health.hediffSet.CountAddedAndImplantedParts() * 1.0f;
+            set { }
+        }
+    }
+}
