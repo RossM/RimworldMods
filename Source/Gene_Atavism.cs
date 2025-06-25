c571c7f4b9b7ee0f519870c1adcd5c7bb9c3a639
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-25 00:35:15 -0700

    Atavism tweaks

--- Source/Gene_Atavism.cs
+++ Source/Gene_Atavism.cs
@@ -9,7 +9,7 @@ namespace XylRacesCore
 {
     public class Gene_Atavism : Gene
     {
-        public Gene addedGene;
+        public List<Gene> addedGenes;
 
         public override void PostAdd()
         {
@@ -17,19 +17,41 @@ namespace XylRacesCore
 
             if (Rand.Chance(0.5f))
             {
-                var geneDef = DefDatabase<GeneDef>.AllDefsListForReading.RandomElementByWeight(GeneWeight);
-                addedGene = pawn.genes.AddGene(geneDef, pawn.genes.Xenogenes.Contains(this));
+                AddGene(DefDatabase<GeneDef>.AllDefsListForReading.RandomElementByWeight(GeneWeight));
+
+                if (Rand.Chance(0.5f))
+                {
+                    AddGene(DefDatabase<GeneDef>.GetNamed("Instability_Mild"));
+                }
             }
         }
 
+        private void AddGene(GeneDef geneDef)
+        {
+            addedGenes ??= new List<Gene>();
+            if (geneDef != null && !pawn.genes.HasActiveGene(geneDef))
+                addedGenes.Add(pawn.genes.AddGene(geneDef, IsXenogene));
+        }
+
+        private bool IsXenogene => pawn.genes.Xenogenes.Contains(this);
+
         float GeneWeight(GeneDef def)
         {
+            // No genes requiring archite capsules
             if (def.biostatArc != 0)
                 return 0.0f;
+            // No purely cosmetic genes
             if (def.biostatCpx == 0)
                 return 0.0f;
-            if (def.biostatMet is < -3 or > 3)
+            // No genes with too large an effect on metabolic efficiency
+            if (def.biostatMet is < -2 or > 2)
                 return 0.0f;
+
+            // No genes with requirements, unless they are met by the pawn's xenotype
+            if (def.prerequisite != null && !pawn.genes.Xenotype.AllGenes.Contains(def.prerequisite))
+                return 0.0f;
+
+            // No genes that conflict with genes in the pawn's xenotype
             foreach (var gene in pawn.genes.Xenotype.AllGenes)
             {
                 if (def == gene)
@@ -37,6 +59,7 @@ namespace XylRacesCore
                 if (def.exclusionTags != null && gene.exclusionTags != null && def.exclusionTags.Union(gene.exclusionTags).Any())
                     return 0.0f;
             }
+
             return def.selectionWeight;
         }
 
@@ -44,8 +67,11 @@ namespace XylRacesCore
         {
             base.PostRemove();
 
-            if (addedGene != null)
-                pawn.genes.RemoveGene(addedGene);
+            if (addedGenes == null) 
+                return;
+
+            foreach (var gene in addedGenes)
+                pawn.genes.RemoveGene(gene);
         }
     }
 }
