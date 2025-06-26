ca09ad9909b88d4d70270995b0cf1c0647974c90
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-26 01:41:52 -0700

    Herd instinct removes penalty for sleeping in barracks

---
+++ Source/Patch_Toils_LayDown.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [HarmonyPatch(typeof(Toils_LayDown), "ApplyBedThoughts")]
+    public class Patch_Toils_LayDown
+    {
+        [HarmonyPostfix]
+        public static void ApplyBedThoughts_Postfix(Pawn actor, Building_Bed bed)
+        {
+            if (bed != null && bed == actor.ownership.OwnedBed && !bed.ForPrisoners && !actor.story.traits.HasTrait(TraitDefOf.Ascetic))
+            {
+                Room room = bed.GetRoom();
+                if (room != null)
+                {
+                    ThoughtDef thoughtDef = null;
+                    if (room.Role == RoomRoleDefOf.Barracks)
+                    {
+                        thoughtDef = DefDatabase<ThoughtDef>.GetNamed("SleptInBarracks_HerdInstinct");
+                    }
+                    if (thoughtDef != null)
+                    {
+                        int scoreStageIndex = RoomStatDefOf.Impressiveness.GetScoreStageIndex(bed.GetRoom().GetStat(RoomStatDefOf.Impressiveness));
+                        if (thoughtDef.stages[scoreStageIndex] != null)
+                        {
+                            actor.needs.mood.thoughts.memories.TryGainMemory(ThoughtMaker.MakeThought(thoughtDef, scoreStageIndex));
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
