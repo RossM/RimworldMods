9be2c0abe23a78d1c86305eef97c95cf5b2a588e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-26 15:04:50 -0700

    Add gender-determining genes

---
+++ Source/Gene_GenderRatio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class Gene_GenderRatio : Gene
+    {
+        public ModExtension_GeneDef_GenderRatio DefExt => def.GetModExtension<ModExtension_GeneDef_GenderRatio>();
+
+        public Gender GetGender()
+        {
+            return Rand.Chance(DefExt.femaleChance) ? Gender.Female : Gender.Male;
+        }
+    }
+
+    public class ModExtension_GeneDef_GenderRatio : DefModExtension
+    {
+        public float femaleChance = 0.5f;
+    }
+}
--- Source/Patch_PawnGenerator.cs
+++ Source/Patch_PawnGenerator.cs
@@ -3,17 +3,91 @@ using RimWorld;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using Verse;
 
 namespace XylRacesCore
 {
-    [HarmonyPatch(typeof(PawnGenerator), nameof(PawnGenerator.GetXenotypeForGeneratedPawn))]
+    [HarmonyPatch(typeof(PawnGenerator))]
     public class Patch_PawnGenerator
     {
-        [HarmonyPostfix]
-        static void Postfix(PawnGenerationRequest request, ref XenotypeDef __result)
+        private static readonly InstructionMatcher FixupTryGenerateNewPawnInternal = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 1,
+                    Replace = true,
+                    Pattern =
+                    [
+                        // PawnBioAndNameGenerator.GiveAppropriateBioAndNameTo(pawn, faction.def, request, xenotype);
+                        // At this point 'pawn is on the top of the stack
+                        CodeInstruction.LoadLocal(0),
+                        CodeInstruction.LoadField(typeof(Faction), "def"),
+                        CodeInstruction.LoadArgument(0),
+                        new CodeInstruction(OpCodes.Ldobj, typeof(PawnGenerationRequest)),
+                        new CodeInstruction(OpCodes.Ldloc_S, 4),
+                        CodeInstruction.Call(typeof(PawnBioAndNameGenerator), "GiveAppropriateBioAndNameTo"), 
+                    ],
+                    Output =
+                    [
+                        // Duplicate 'pawn'
+                        new CodeInstruction(OpCodes.Dup),
+                        // Load 'request'
+                        CodeInstruction.LoadArgument(0),
+                        // new CodeInstruction(OpCodes.Ldobj, typeof(PawnGenerationRequest)),
+                        // Load 'xenotype'
+                        new CodeInstruction(OpCodes.Ldloc_S, 4),
+                        // Call our fixup
+                        CodeInstruction.Call(() => ModifyGenderByGenes),
+
+                        // Emit the original call
+                        CodeInstruction.LoadLocal(0),
+                        CodeInstruction.LoadField(typeof(Faction), "def"),
+                        CodeInstruction.LoadArgument(0),
+                        new CodeInstruction(OpCodes.Ldobj, typeof(PawnGenerationRequest)),
+                        new CodeInstruction(OpCodes.Ldloc_S, 4),
+                        CodeInstruction.Call(typeof(PawnBioAndNameGenerator), "GiveAppropriateBioAndNameTo"),
+
+                    ],
+                }
+            }
+        };
+
+        [HarmonyTranspiler, HarmonyPatch("TryGenerateNewPawnInternal")]
+        static IEnumerable<CodeInstruction> TryGenerateNewPawnInternal_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            if (!FixupTryGenerateNewPawnInternal.MatchAndReplace(ref instructionsList, out string reason, generator))
+                Log.Error(string.Format("XylRacesCore.Patch_StatWorker.GetOffsetsAndFactorsExplanation_Transpiler: {0}", reason));
+            return instructionsList;
+        }
+
+        static void ModifyGenderByGenes(Pawn pawn, ref PawnGenerationRequest request, XenotypeDef xenotype)
+        {
+            //Log.Message(string.Format("ModifyGenderByGenes: pawn = {0}, request = ({1}), xenotype = {2}", pawn, request, xenotype));
+            //Log.Message(string.Format("  ForcedCustomXenotype = {0}", request.ForcedCustomXenotype));
+            var gene = request.ForcedEndogenes?.FirstOrDefault(HasGenderRatio) ??
+                       request.ForcedXenogenes?.FirstOrDefault(HasGenderRatio) ??
+                       request.ForcedCustomXenotype?.genes.FirstOrDefault(HasGenderRatio) ?? 
+                       xenotype.AllGenes.FirstOrDefault(HasGenderRatio);
+            if (gene != null)
+            {
+                pawn.gender = Rand.Chance(gene.GetModExtension<ModExtension_GeneDef_GenderRatio>().femaleChance)
+                    ? Gender.Female : Gender.Male;
+            }
+        }
+
+        static bool HasGenderRatio(GeneDef gene)
+        {
+            return gene.GetModExtension<ModExtension_GeneDef_GenderRatio>() != null;
+        }
+
+        [HarmonyPostfix, HarmonyPatch(nameof(PawnGenerator.GetXenotypeForGeneratedPawn))]
+        static void GetXenotypeForGeneratedPawn_Postfix(PawnGenerationRequest request, ref XenotypeDef __result)
         {
             if (Find.Scenario != null)
             {
