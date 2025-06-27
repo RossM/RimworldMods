f7e8efb8b4155bbce6a64b8b3b01681256d7f4ce
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-27 05:28:22 -0700

    Rework FoodHelpers
    I now understand how FoodTypeFlags works.

--- Source/FoodHelpers.cs
+++ Source/FoodHelpers.cs
@@ -5,98 +5,112 @@ namespace XylRacesCore;
 
 public static class FoodHelpers
 {
+    public enum FoodType
+    {
+        None,
+        Meat,
+        NonMeat,
+        Fungus,
+        AnimalProduct,
+    }
+
+    public static FoodType GetBestFoodType(ThingDef foodDef)
+    {
+        FoodTypeFlags flags = foodDef.ingestible?.foodType ?? 0;
+
+        if (flags == FoodTypeFlags.Fungus)
+            return FoodType.Fungus;
+        if ((flags & FoodTypeFlags.AnimalProduct) != 0)
+            return FoodType.AnimalProduct;
+        if ((flags & (FoodTypeFlags.VegetableOrFruit | FoodTypeFlags.Plant | FoodTypeFlags.Seed)) != 0)
+            return FoodType.NonMeat;
+        if ((flags & (FoodTypeFlags.Meat | FoodTypeFlags.Corpse)) != 0)
+            return FoodType.Meat;
+        return FoodType.None;
+    }
+
     public static float GetExtraNutritionFactor(Pawn eater, Thing foodSource, ThingDef foodDef)
     {
         if (!foodDef.IsProcessedFood)
         {
-            var foodKind = FoodUtility.GetFoodKind(foodSource);
-
-            if (foodDef.IsFungus)
-                return eater.GetStatValue("RawFungusNutritionFactor", 1.0f) * 
-                       eater.GetStatValue("RawNonMeatNutritionFactor", 1.0f);
-
-            if (foodKind == FoodKind.Meat)
-                return eater.GetStatValue("RawMeatNutritionFactor", 1.0f);
-
-            if (foodDef.IsAnimalProduct)
-                return eater.GetStatValue("RawAnimalProductNutritionFactor", 1.0f);
-
-            if (foodKind == FoodKind.NonMeat)
-                return eater.GetStatValue("RawNonMeatNutritionFactor", 1.0f);
-
-            return 1.0f;
+            return GetBestFoodType(foodDef) switch
+            {
+                FoodType.Fungus => eater.GetStatValue("RawFungusNutritionFactor", 1.0f) *
+                                   eater.GetStatValue("RawNonMeatNutritionFactor", 1.0f),
+                FoodType.Meat => eater.GetStatValue("RawMeatNutritionFactor", 1.0f),
+                FoodType.AnimalProduct => eater.GetStatValue("RawAnimalProductNutritionFactor", 1.0f),
+                FoodType.NonMeat => eater.GetStatValue("RawNonMeatNutritionFactor", 1.0f),
+                _ => 1.0f
+            };
         }
         else
         {
             var compIngredients = foodSource.TryGetComp<CompIngredients>();
-            if (compIngredients != null)
+            if (compIngredients == null) 
+                return 1.0f;
+
+            var hasMeat = false;
+            var hasAnimalProduct = false;
+            var hasNonMeat = false;
+
+            foreach (var ingredient in compIngredients.ingredients)
             {
-                var hasMeat = false;
-                var hasAnimalProduct = false;
-                var hasNonMeat = false;
-
-                foreach (var ingredient in compIngredients.ingredients)
+                switch (GetBestFoodType(ingredient))
                 {
-                    var foodKind = FoodUtility.GetFoodKind(ingredient);
-
-                    if (foodKind == FoodKind.Meat)
+                    case FoodType.Meat:
                         hasMeat = true;
-                    if (foodKind == FoodKind.NonMeat)
+                        break;
+                    case FoodType.NonMeat:
+                    case FoodType.Fungus:
                         hasNonMeat = true;
-                    if (ingredient.IsAnimalProduct)
+                        break;
+                    case FoodType.AnimalProduct:
                         hasAnimalProduct = true;
+                        break;
                 }
-
-                var multiplier = 0.0f;
-                var divisor = 0.0f;
-
-                if (hasMeat)
-                {
-                    multiplier += eater.GetStatValue("CookedMeatNutritionFactor", 1.0f);
-                    divisor += 1.0f;
-                }
-
-                if (hasAnimalProduct)
-                {
-                    multiplier += eater.GetStatValue("CookedAnimalProductNutritionFactor", 1.0f);
-                    divisor += 1.0f;
-                }
-
-                if (hasNonMeat)
-                {
-                    multiplier += eater.GetStatValue("CookedNonMeatNutritionFactor", 1.0f);
-                    divisor += 1.0f;
-                }
-
-                return divisor > 0 ? multiplier / divisor : 1.0f;
             }
 
-            return 1.0f;
+            var multiplier = 0.0f;
+            var divisor = 0.0f;
+
+            if (hasMeat)
+            {
+                multiplier += eater.GetStatValue("CookedMeatNutritionFactor", 1.0f);
+                divisor += 1.0f;
+            }
+
+            if (hasAnimalProduct)
+            {
+                multiplier += eater.GetStatValue("CookedAnimalProductNutritionFactor", 1.0f);
+                divisor += 1.0f;
+            }
+
+            if (hasNonMeat)
+            {
+                multiplier += eater.GetStatValue("CookedNonMeatNutritionFactor", 1.0f);
+                divisor += 1.0f;
+            }
+
+            return divisor > 0 ? multiplier / divisor : 1.0f;
+
         }
     }
+
     public static float GetFoodPoisoningChanceOffset(Pawn eater, Thing foodSource)
     {
         var foodDef = foodSource.def;
 
         if (foodDef.IsProcessedFood) 
             return 0.0f;
-        
-        var foodKind = FoodUtility.GetFoodKind(foodSource);
 
-        if (foodDef.IsFungus)
-            return eater.GetStatValue("RawFungusFoodPoisoningChanceOffset", 0.0f) +
-                   eater.GetStatValue("RawNonMeatFoodPoisoningChanceOffset", 0.0f);
-
-        if (foodKind == FoodKind.Meat)
-            return eater.GetStatValue("RawMeatFoodPoisoningChanceOffset", 0.0f);
-
-        if (foodDef.IsAnimalProduct)
-            return eater.GetStatValue("RawAnimalProductFoodPoisoningChanceOffset", 0.0f);
-
-        if (foodKind == FoodKind.NonMeat)
-            return eater.GetStatValue("RawNonMeatFoodPoisoningChanceOffset", 0.0f);
-
-        return 0.0f;
-
+        return GetBestFoodType(foodSource.def) switch
+        {
+            FoodType.Fungus => eater.GetStatValue("RawFungusFoodPoisoningChanceOffset", 0.0f) +
+                               eater.GetStatValue("RawNonMeatFoodPoisoningChanceOffset", 0.0f),
+            FoodType.Meat => eater.GetStatValue("RawMeatFoodPoisoningChanceOffset", 0.0f),
+            FoodType.AnimalProduct => eater.GetStatValue("RawAnimalProductFoodPoisoningChanceOffset", 0.0f),
+            FoodType.NonMeat => eater.GetStatValue("RawNonMeatFoodPoisoningChanceOffset", 0.0f),
+            _ => 0.0f
+        };
     }
 }
