dd7d5e45ec9621ebb486ff93018cdd9317da225e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-06-30 19:09:55 -0700

    Better flight UI

--- Source/Gene_Flight.cs
+++ Source/Gene_Flight.cs
@@ -32,8 +32,7 @@ namespace XylRacesCore
 
     public class Gene_Flight : Gene
     {
-        public bool allowFlight = false;
-        public bool flyOnlyWhenDrafted = false;
+        public bool autoFly = false;
 
         [Unsaved(false)] private bool wasFlying;
 
@@ -42,8 +41,7 @@ namespace XylRacesCore
         public override void ExposeData()
         {
             base.ExposeData();
-            Scribe_Values.Look(ref allowFlight, "allowFlight", false);
-            Scribe_Values.Look(ref flyOnlyWhenDrafted, "flyOnlyWhenDrafted", false);
+            Scribe_Values.Look(ref autoFly, "autoFly", false);
         }
 
 
@@ -56,26 +54,27 @@ namespace XylRacesCore
             if (!pawn.IsColonistPlayerControlled)
                 yield break;
 
-            yield return new Command_Toggle
+            yield return new Command_ActionWithCooldown()
             {
-                defaultLabel = "Allow flight",
-                defaultDesc = "Allow this character to fly.",
-                isActive = () => allowFlight,
-                toggleAction = () => { allowFlight = !allowFlight; },
+                action = () =>
+                {
+                    pawn.flight.StartFlying();
+                },
+                defaultLabel = "Fly",
+                defaultDesc = "Start flying. Flying grants increased movement speed and allows travelling over some obstacles.",
+                Disabled = !pawn.flight.CanFlyNow,
+                cooldownPercentGetter = () => 1.0f - pawn.flight.Get<int>("flightCooldownTicks") / (pawn.GetStatValue(StatDefOf.FlightCooldown) * 60f),
                 icon = DefExt.Icon,
             };
 
-            if (allowFlight)
+            yield return new Command_Toggle
             {
-                yield return new Command_Toggle
-                {
-                    defaultLabel = "Fly only when drafted",
-                    defaultDesc = "Restrict this character to only fly when drafted.",
-                    isActive = () => flyOnlyWhenDrafted,
-                    toggleAction = () => { flyOnlyWhenDrafted = !flyOnlyWhenDrafted; },
-                    icon = DefExt.Icon,
-                };
-            }
+                defaultLabel = "Auto-fly",
+                defaultDesc = "Automatically fly when moving.",
+                isActive = () => autoFly,
+                toggleAction = () => { autoFly = !autoFly; },
+                icon = DefExt.Icon,
+            };
         }
 
         public void Notify_JobStarted(Job job)
@@ -102,17 +101,8 @@ namespace XylRacesCore
             if (!flight.CanEverFly)
                 return;
 
-            bool tryFlying = allowFlight && (!flyOnlyWhenDrafted || pawn.Drafted);
-            if (flight.Flying)
-            {
-                if (!tryFlying)
-                    flight.ForceLand();;
-            }
-            else
-            {
-                if (tryFlying)
-                    flight.StartFlying();
-            }
+            if (!flight.Flying && autoFly && pawn.pather.Moving)
+                flight.StartFlying();
         }
 
         // If a downed flying pawn lands on a non-walkable tile, they are killed and their corpse destroyed.
--- Source/ReflectionHelpers.cs
+++ Source/ReflectionHelpers.cs
@@ -11,7 +11,8 @@ namespace XylRacesCore
 {
     public static class ReflectionHelpers
     {
-        private static readonly Dictionary<(Type, string), object> getAccessors = new ();
+        private static readonly Dictionary<(Type, string), object> getAccessors = new();
+        private static readonly Dictionary<(Type, string), object> setAccessors = new();
 
         public static TResult Get<TResult>(this object obj, string fieldName)
         {
@@ -20,16 +21,38 @@ namespace XylRacesCore
 
         public static Func<object, TResult> GetAccessor<TResult>(Type type, string fieldName)
         {
-            if (!getAccessors.TryGetValue((type, fieldName), out var accessor))
+            if (!getAccessors.TryGetValue((type, fieldName), out object accessor))
             {
                 FieldInfo fieldDef = AccessTools.Field(type, fieldName);
-                ParameterExpression parameter = Expression.Variable(typeof(object), "obj");
+                ParameterExpression objParameter = Expression.Variable(typeof(object), "obj");
                 accessor = Expression.Lambda<Func<object, TResult>>(
-                    Expression.MakeMemberAccess(Expression.Convert(parameter, type), fieldDef), parameter).Compile();
+                    Expression.MakeMemberAccess(Expression.Convert(objParameter, type), fieldDef), objParameter).Compile();
                 getAccessors.Add((type, fieldName), accessor);
             }
 
             return (Func<object, TResult>)accessor;
+        }
+
+        public static void Set<TResult>(this object obj, string fieldName, TResult value)
+        {
+            SetAccessor<TResult>(obj.GetType(), fieldName)(obj, value);
+        }
+
+        public static Action<object, TResult> SetAccessor<TResult>(Type type, string fieldName)
+        {
+            if (!setAccessors.TryGetValue((type, fieldName), out object accessor))
+            {
+                FieldInfo fieldDef = AccessTools.Field(type, fieldName);
+                ParameterExpression objParameter = Expression.Variable(typeof(object), "obj");
+                ParameterExpression valueParameter = Expression.Variable(typeof(TResult), "value");
+                accessor = Expression.Lambda<Action<object, TResult>>(
+                    Expression.Assign(
+                        Expression.MakeMemberAccess(Expression.Convert(objParameter, type), fieldDef), valueParameter),
+                    objParameter, valueParameter).Compile();
+                getAccessors.Add((type, fieldName), setAccessors);
+            }
+
+            return (Action<object, TResult>)accessor;
         }
     }
 }
