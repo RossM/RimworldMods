ceca5811a5b6db4695ca48dbcacd77a0ed39cad0
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-02 07:43:34 -0700

    Code cleanup

--- Source/PawnRenderNode_Wings.cs
+++ Source/PawnRenderNode_Wings.cs
@@ -9,20 +9,14 @@ using Verse;
 namespace XylRacesCore
 {
     [UsedImplicitly]
-    public class PawnRenderNode_Wings : PawnRenderNode
+    public class PawnRenderNode_Wings(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+        : PawnRenderNode(pawn, props, tree)
     {
-        public PawnRenderNode_Wings(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree) : base(pawn, props, tree)
-        {
-        }
-
         protected override string TexPathFor(Pawn pawn)
         {
             if (!props.texPaths.NullOrEmpty())
             {
-                if (pawn.flight?.Flying == true)
-                    return props.texPaths[1];
-                else
-                    return props.texPaths[0];
+                return pawn.flight?.Flying == true ? props.texPaths[1] : props.texPaths[0];
             }
 
             return base.TexPathFor(pawn);
