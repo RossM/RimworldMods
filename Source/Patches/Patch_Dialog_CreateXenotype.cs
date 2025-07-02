067ba72fce9ca7b5baeb2347c4311a690aa62a7e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-02 04:13:44 -0700

    Hide gender ratio genes when not inheritable
    They have no effect unless inheritable.

---
+++ Source/Patches/Patch_Dialog_CreateXenotype.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using XylRacesCore.Genes;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(Dialog_CreateXenotype))]
+    public class Patch_Dialog_CreateXenotype
+    {
+        private static readonly InstructionMatcher Fixup_DrawGenes = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 0,
+                    Mode = InstructionMatcher.OutputMode.InsertAfter,
+                    Pattern =
+                    [
+                        CodeInstruction.Call(typeof(GeneUtility), "get_" + nameof(GeneUtility.GenesInOrder)),
+                    ],
+                    Output =
+                    [
+                        // Load this
+                        CodeInstruction.LoadArgument(0),
+                        // Load this.inheritable
+                        CodeInstruction.LoadField(typeof(Dialog_CreateXenotype), "inheritable"),
+                        // Call
+                        CodeInstruction.Call(() => FilterGenes),
+                    ]
+                }
+            }
+        };
+
+        private static List<GeneDef> FilterGenes(List<GeneDef> genes, bool inheritable)
+        {
+            return genes.Where(ShouldBeVisible).ToList();
+
+            bool ShouldBeVisible(GeneDef g)
+            {
+                var defExt = g.GetModExtension<GeneDefExtension_UIFilter>();
+                if (defExt?.inheritable == null)
+                    return true;
+                return defExt.inheritable.Value == inheritable;
+            }
+        }
+
+        [HarmonyTranspiler, UsedImplicitly, HarmonyPatch("DrawGenes")]
+        public static IEnumerable<CodeInstruction> DrawGenes_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            if (!Fixup_DrawGenes.MatchAndReplace(ref instructionsList, out string reason, generator))
+                Log.Error(string.Format("{0}: {1}", MethodBase.GetCurrentMethod().FullDescription(), reason));
+            return instructionsList;
+        }
+
+    }
+}
