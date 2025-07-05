edd68deb428f13e8fe09d699d730eab6c6c32bfe
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-04 20:27:07 -0700

    Add scaleskin body graphics

---
+++ Source/PawnRenderNode_NotFur.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class PawnRenderNode_NotFur(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+        : PawnRenderNode(pawn, props, tree)
+    {
+        public override Graphic GraphicFor(Pawn pawn)
+        {
+            if (!ModLister.CheckBiotech("Fur"))
+            {
+                return null;
+            }
+            if (pawn.story?.furDef == null)
+            {
+                return null;
+            }
+            return GraphicDatabase.Get<Graphic_Multi>(pawn.story?.furDef.GetFurBodyGraphicPath(pawn), ShaderFor(pawn), Vector2.one, ColorFor(pawn));
+        }
+    }
+}
