8aaa7d8c1a183800f7b2e80a843e053cfd44cf80
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-09 11:53:34 -0700

    Remove unused usings

--- Source/JobDriver_Soak.cs
+++ Source/JobDriver_Soak.cs
@@ -1,10 +1,6 @@
 ﻿using JetBrains.Annotations;
 using RimWorld;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 using Verse.AI;
 
