ea5850b0c9805820cd712d33f2eaee13a8f27ab3
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-09 04:44:27 -0700

    Ensure reserve pawns are appropriate xenotype

---
+++ Source/ScenPart_ConfigPage_ConfigureStartingPawns_Xenotypes_Ext.cs
@@ -0,0 +1,34 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace XylRacesCore
+{
+    [UsedImplicitly]
+    public class ScenPart_ConfigPage_ConfigureStartingPawns_Xenotypes_Ext : ScenPart_ConfigPage_ConfigureStartingPawns_Xenotypes
+    {
+        protected override void GenerateStartingPawns()
+        {
+            base.GenerateStartingPawns();
+
+            if (Find.GameInitData.startingAndOptionalPawns.Count >= pawnChoiceCount)
+                return;
+
+            // Make sure that the extra reserve pawns are an appropriate xenotype
+            int index = Find.GameInitData.startingAndOptionalPawns.Count;
+            var request = StartingPawnUtility.GetGenerationRequest(index);
+            request.ForcedXenotype = xenotypeCounts[xenotypeCounts.Count - 1].xenotype;
+            StartingPawnUtility.SetGenerationRequest(index, request);
+
+            while (Find.GameInitData.startingAndOptionalPawns.Count < pawnChoiceCount)
+            {
+                StartingPawnUtility.AddNewPawn(index);
+            }
+        }
+    }
+}
