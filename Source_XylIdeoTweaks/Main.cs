2253c0cf95b519c71153a67351f6be84342ffa95
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-12 01:58:47 -0700

    New skeleton mod

---
+++ Source_XylIdeoTweaks/Main.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using HarmonyLib;
+using Verse;
+
+namespace Source_ExposableChecker
+{
+    [UsedImplicitly]
+    [StaticConstructorOnStartup]
+    public class Main(ModContentPack content) : Mod(content)
+    {
+        static Main()
+        {
+            var harmony = new Harmony("net.pardeike.rimworld.lib.harmony");
+            harmony.PatchAll();
+        }
+    }
+}
