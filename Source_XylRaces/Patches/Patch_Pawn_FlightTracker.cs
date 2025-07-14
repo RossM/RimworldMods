be924bf5b744611b5964b18fab3e15a2cf1d26e2
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-07-14 07:40:58 -0700

    Use Publicizer (correctly this time)

--- Source_XylRaces/Patches/Patch_Pawn_FlightTracker.cs
+++ Source_XylRaces/Patches/Patch_Pawn_FlightTracker.cs
@@ -15,7 +15,7 @@ namespace XylRacesCore.Patches
         {
             using (new ProfileBlock())
             {
-                var pawn = __instance.Get<Pawn>("pawn");
+                var pawn = __instance.pawn;
                 if (pawn.IsPlayerControlled && pawn.genes?.GetFirstGeneOfType<Flight>() is not null)
                 {
                     return false;
@@ -30,7 +30,7 @@ namespace XylRacesCore.Patches
         {
             using (new ProfileBlock())
             {
-                var pawn = __instance.Get<Pawn>("pawn");
+                var pawn = __instance.pawn;
                 if (pawn.Downed && !pawn.Position.WalkableBy(pawn.Map, pawn))
                 {
                     if (pawn.IsPlayerControlled && pawn.genes?.GetFirstGeneOfType<Flight>() is { } gene)
