fb1fbd76f56899383c11aa86aff5f28db38598a1
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2025-08-20 03:44:17 -0700

    Fix namespace

--- Source_XylRaces/Patches/Patch_Pawn_GeneTracker.cs
+++ Source_XylRaces/Patches/Patch_Pawn_GeneTracker.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HarmonyLib;
+﻿using HarmonyLib;
 using JetBrains.Annotations;
 using RimWorld;
-using Verse.AI;
-using XylRacesCore;
 
-namespace Source_XylRaces.Patches
+namespace XylRacesCore.Patches
 {
     [HarmonyPatch(typeof(Pawn_GeneTracker))]
     public static class Patch_Pawn_GeneTracker
