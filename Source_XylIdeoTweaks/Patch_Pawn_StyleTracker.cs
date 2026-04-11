70417556314794ac24a617c070162b174ca63020
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-11 04:03:06 -0700

    Stop pawns from getting into a style change loop when their genes prevent them from using a preferred hair/beard style

---
+++ Source_XylIdeoTweaks/Patch_Pawn_StyleTracker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+using XylRacesCore;
+
+namespace Source_XylIdeoTweaks
+{
+    [HarmonyPatch(typeof(Pawn_StyleTracker))]
+    public class Patch_Pawn_StyleTracker
+    {
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(Pawn_StyleTracker.HasUnwantedBeard), MethodType.Getter)]
+        public static void HasUnwantedBeard_Postfix(Pawn_StyleTracker __instance, ref bool __result)
+        {
+            if (__result)
+            {
+                // Only want a style change if there are any valid options to change to
+                __result = DefDatabase<BeardDef>.AllDefs.Any(beardDef => PawnStyleItemChooser.WantsToUseStyle(__instance.pawn, beardDef));
+            }
+        }
+
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(Pawn_StyleTracker.HasUnwantedHairStyle), MethodType.Getter)]
+        public static void HasUnwantedHairStyle_Postfix(Pawn_StyleTracker __instance, ref bool __result)
+        {
+            if (__result)
+            {
+                // Only want a style change if there are any valid options to change to
+                __result = DefDatabase<HairDef>.AllDefs.Any(hairDef => PawnStyleItemChooser.WantsToUseStyle(__instance.pawn, hairDef));
+            }
+        }
+    }
+}
