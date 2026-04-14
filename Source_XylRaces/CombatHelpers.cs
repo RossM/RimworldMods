39969527e8132dc6d2dfb2ee80210f296a786fd3
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-14 11:02:30 -0700

    Adjust Precognition. Now gives +15 (20%) melee and ranged dodge chance

---
+++ Source_XylRaces/CombatHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    public static class CombatHelpers
+    {
+
+        [DefOf]
+        public static class Defs
+        {
+            [UsedImplicitly] public static StatDef XylRangedDodgeChance;
+        }
+
+        public static float GetRangedDodgeChance(Pawn target)
+        {
+            return target.GetStatValue(Defs.XylRangedDodgeChance);
+        }
+    }
+}
---
+++ Source_XylRaces/Patches/Patch_ShotReport.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(ShotReport))]
+    public static class Patch_ShotReport
+    {
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(ShotReport.AimOnTargetChance_IgnoringPosture), MethodType.Getter)]
+        public static void AimOnTargetChance_IgnoringPosture_Postfix(ShotReport __instance, ref float __result)
+        {
+            if (__instance.target.Thing is Pawn targetPawn)
+            {
+                float rangedDodgeChance = CombatHelpers.GetRangedDodgeChance(targetPawn);
+                if (rangedDodgeChance > 0)
+                    __result = Math.Min(__result, 1.0f) * (1.0f - rangedDodgeChance);
+            }
+        }
+
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(ShotReport.GetTextReadout))]
+        public static void GetTextReadout_Postfix(ShotReport __instance, ref string __result)
+        {
+            if (__instance.target.Thing is Pawn targetPawn) 
+            {
+                float rangedDodgeChance = CombatHelpers.GetRangedDodgeChance(targetPawn);
+                if (rangedDodgeChance > 0)
+                {
+                    StringBuilder sb = new StringBuilder(__result);
+                    sb.AppendLine("   " + CombatHelpers.Defs.XylRangedDodgeChance.LabelCap + ": " + rangedDodgeChance.ToStringPercent());
+                    __result = sb.ToString();
+                }
+            }
+        }
+    }
+}
