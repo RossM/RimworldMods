3a755af2fb7a07b7788d57121fca022f017ec558
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-15 07:15:18 -0700

    Allow characters with fungus eater gene to build fungal gravel

---
+++ Source_XylRaces/Patches/Patch_DesignationCategoryDef.cs
@@ -0,0 +1,56 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using XylRacesCore.Genes;
+using static Verse.DesignationCategoryDef;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(Verse.DesignationCategoryDef))]
+    public static class Patch_DesignationCategoryDef
+    {
+        [HarmonyPostfix, UsedImplicitly,
+         HarmonyPatch(nameof(DesignationCategoryDef.ResolvedAllowedDesignators), MethodType.Getter)]
+        public static void ResolvedAllowedDesignators_Postfix(DesignationCategoryDef __instance, ref IEnumerable<Designator> __result)
+        {
+            AddDesignators(__instance, ref __result);
+        }
+
+        [HarmonyPostfix, UsedImplicitly,
+         HarmonyPatch(nameof(DesignationCategoryDef.AllResolvedAndIdeoDesignators), MethodType.Getter)]
+        public static void AllResolvedAndIdeoDesignators_Postfix(DesignationCategoryDef __instance, ref IEnumerable<Designator> __result)
+        {
+            AddDesignators(__instance, ref __result);
+        }
+
+        private static void AddDesignators(DesignationCategoryDef __instance, ref IEnumerable<Designator> __result)
+        {
+            HashSet<Designator> geneDesignators = new();
+
+            foreach (var defExtension_designator in Faction.OfPlayer.GetPawns().SelectMany(pawn => pawn.ActiveGeneDefExtensionsOfType<GeneDefExtension_Designator>()))
+            {
+                geneDesignators.AddRange(defExtension_designator.addDesignators.Where(def => def.designationCategory == __instance).Select(GetCachedDesignator));
+            }
+
+            if (geneDesignators.Any())
+                __result = __result.Concat(geneDesignators);
+
+            Designator GetCachedDesignator(BuildableDef def)
+            {
+                BuildablePreceptBuilding key = new BuildablePreceptBuilding(def, null);
+                if (!__instance.ideoBuildingDesignatorsCached.TryGetValue(key, out var value))
+                {
+                    value = new Designator_Build(def);
+                    __instance.ideoBuildingDesignatorsCached[key] = value;
+                }
+                return value;
+            }
+        }
+    }
+}
---
+++ Source_XylRaces/Patches/Patch_GenConstruct.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using XylRacesCore.Genes;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(GenConstruct))]
+    public static class Patch_GenConstruct
+    {
+        private static readonly InstructionMatcher Fixup_CanConstruct = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 1,
+                    Mode = InstructionMatcher.OutputMode.Replace,
+                    Pattern =
+                    [
+                        CodeInstruction.LoadArgument(1),
+                        CodeInstruction.Call(typeof(Pawn), "get_" + nameof(Pawn.Ideo)),
+                        CodeInstruction.LoadArgument(0),
+                        CodeInstruction.Call(typeof(Ideo), nameof(Ideo.MembersCanBuild)),
+                    ],
+                    Output =
+                    [
+                        CodeInstruction.LoadArgument(0),
+                        CodeInstruction.LoadArgument(1),
+                        CodeInstruction.Call(() => CanBuildHelper),
+                    ]
+                }
+            }
+        };
+
+        [HarmonyTranspiler, UsedImplicitly, HarmonyPatch(nameof(GenConstruct.CanConstruct), [typeof(Thing), typeof(Pawn), typeof(bool), typeof(bool), typeof(JobDef)])]
+        public static IEnumerable<CodeInstruction> CanConstruct_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            Fixup_CanConstruct.MatchAndReplace(ref instructionsList, generator);
+            return instructionsList;
+        }
+
+        public static bool CanBuildHelper(Thing thing, Pawn pawn)
+        {
+            if (pawn.Ideo.MembersCanBuild(thing))
+                return true;
+
+            BuildableDef def = thing.def.entityDefToBuild ?? thing.def;
+
+            var result = pawn.ActiveGeneDefExtensionsOfType<GeneDefExtension_Designator>().Any(defExtension_designator => defExtension_designator.addDesignators.Contains(def));
+            if (!result)
+            {
+                var geneNames = DefDatabase<GeneDef>.AllDefs
+                    .Where(gene => gene.GetModExtension<GeneDefExtension_Designator>()?.addDesignators.Contains(def) ?? false)
+                    .Select(gene => gene.LabelCap.ToString()).ToList();
+
+                if (geneNames.Count > 0)
+                    JobFailReason.Is("Only characters with the {0} gene can build", geneNames.ToCommaList());
+            }
+
+            return result;
+        }
+    }
+}
