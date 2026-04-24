a4ed5e30015e87b148831ec575609f134b63f34e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-24 07:54:22 -0700

    Show which gene caused a thought

---
+++ Source_XylRaces/Patches/Patch_Thought.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(Thought))]
+    public static class Patch_Thought
+    {
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(Thought.Description), MethodType.Getter)]
+        public static void Description_Postfix(Thought __instance, ref string __result)
+        {
+            GeneDef sourceGene = __instance.def.requiredGenes.EmptyIfNull().FirstOrDefault(geneDef => __instance.pawn.HasActiveGene(geneDef));
+            if (sourceGene == null) 
+                return;
+
+            // This is a minor UI improvement to show which gene caused a thought
+            __result += "\n\n" + ("IncapableOfTooltipGene".Translate() + ": " + sourceGene.LabelCap).Colorize(ColoredText.GeneColor);
+        }
+    }
+}
