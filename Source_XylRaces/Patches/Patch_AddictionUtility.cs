66033849d4ef0453bee932eaca17050215fcca03
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-25 02:51:17 -0700

    New nixie gene: drug sensitive. Move focus gene to nixie.

---
+++ Source_XylRaces/Patches/Patch_AddictionUtility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(AddictionUtility))]
+    public static class Patch_AddictionUtility
+    {
+        [DefOf]
+        private static class Defs
+        {
+            [UsedImplicitly] public static StatDef XylDrugEffectMultiplier;
+        }
+
+        [HarmonyPostfix, UsedImplicitly,
+         HarmonyPatch(nameof(AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize))]
+        public static void ModifyChemicalEffectForToleranceAndBodySize_Postfix(Pawn pawn, ref float effect,
+            bool applyGeneToleranceFactor, bool divideByBodySize)
+        {
+            using (new ProfileBlock())
+            {
+                effect *= pawn.GetStatValue(Defs.XylDrugEffectMultiplier);
+            }
+        }
+    }
+}
--- Source_XylRaces/Patches/Patch_Pawn_GeneTracker.cs
+++ Source_XylRaces/Patches/Patch_Pawn_GeneTracker.cs
@@ -7,10 +7,25 @@ namespace XylRacesCore.Patches
     [HarmonyPatch(typeof(Pawn_GeneTracker))]
     public static class Patch_Pawn_GeneTracker
     {
+        [DefOf]
+        private static class Defs
+        {
+            [UsedImplicitly] public static StatDef XylGlobalAddictionChanceFactor;
+        }
+
         [HarmonyPostfix, UsedImplicitly, HarmonyPatch("Notify_GenesChanged")]
         public static void Notify_GenesChanged_Postfix(Pawn_GeneTracker __instance)
         {
             __instance.pawn.GetComp<CompPawn_LookupCache>()?.Notify_GenesChanged();
+        }
+
+        [HarmonyPostfix, UsedImplicitly, HarmonyPatch(nameof(Pawn_GeneTracker.AddictionChanceFactor))]
+        public static void AddictionChanceFactor_Postfix(Pawn_GeneTracker __instance, ref float __result)
+        {
+            using (new ProfileBlock())
+            {
+                __result *= __instance.pawn.GetStatValue(Defs.XylGlobalAddictionChanceFactor);
+            }
         }
     }
 }
