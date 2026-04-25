d67e22179113d597247e6d633a147d5508b14213
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-04-25 05:56:39 -0700

    Add conversion power modifiers for ideos agreeing or disagreeing with xenotype

---
+++ Source_XylRaces/Patches/Patch_ConversionUtility.cs
@@ -0,0 +1,116 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(ConversionUtility))]
+    public static class Patch_ConversionUtility
+    {
+        private static readonly InstructionMatcher Fixup_ConversionPowerFactor_MemesVsTraits = new()
+        {
+            Rules =
+            {
+                new()
+                {
+                    Min = 1, Max = 0,
+                    Mode = InstructionMatcher.OutputMode.InsertBefore,
+                    Pattern =
+                    [
+                        new CodeInstruction(OpCodes.Ldc_R4, -0.4f),
+                        CodeInstruction.Call(typeof(Mathf), nameof(Mathf.Max), [typeof(float), typeof(float)]),
+                    ],
+                    Output =
+                    [
+                        // + OffsetFromXenotype(initiator, false, sb)
+                        CodeInstruction.LoadArgument(0),
+                        new CodeInstruction(OpCodes.Ldc_I4_0),
+                        CodeInstruction.LoadArgument(2),
+                        CodeInstruction.Call(() => OffsetFromXenotype),
+                        new CodeInstruction(OpCodes.Add),
+                        // + OffsetFromXenotype(recipient, true, sb)
+                        CodeInstruction.LoadArgument(1),
+                        new CodeInstruction(OpCodes.Ldc_I4_1),
+                        CodeInstruction.LoadArgument(2),
+                        CodeInstruction.Call(() => OffsetFromXenotype),
+                        new CodeInstruction(OpCodes.Add),
+                    ]
+                }
+            }
+        };
+
+        [HarmonyTranspiler, UsedImplicitly, HarmonyPatch(nameof(ConversionUtility.ConversionPowerFactor_MemesVsTraits))]
+        public static IEnumerable<CodeInstruction> ConversionPowerFactor_MemesVsTraits_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            Fixup_ConversionPowerFactor_MemesVsTraits.MatchAndReplace(ref instructionsList, generator);
+            return instructionsList;
+        }
+
+        public static float OffsetFromXenotype(Pawn pawn, bool invert, StringBuilder sb)
+        {
+            using (new ProfileBlock())
+            {
+                float result = 0;
+                string text = string.Empty;
+                XenotypeDef pawnXenotype = pawn.genes?.Xenotype;
+                if (pawnXenotype == null)
+                    return 0;
+
+                var agreeingMemes = pawnXenotype.GetModExtension<XenotypeDefExtension>()?.agreeingMemes;
+                if (agreeingMemes != null)
+                {
+                    foreach (MemeDef meme in pawn.Ideo.memes)
+                    {
+                        if (agreeingMemes.Contains(meme))
+                        {
+                            float offset = invert ? -0.2f : 0.2f;
+                            result += offset;
+                            text += MemeAndXenotypeDesc(meme, pawnXenotype, offset);
+                        }
+                    }
+                }
+
+                var disagreeingMemes = pawnXenotype.GetModExtension<XenotypeDefExtension>()?.disagreeingMemes;
+                if (disagreeingMemes != null)
+                {
+                    foreach (MemeDef meme in pawn.Ideo.memes)
+                    {
+                        if (disagreeingMemes.Contains(meme))
+                        {
+                            float offset = invert ? 0.2f : -0.2f;
+                            result += offset;
+                            text += MemeAndXenotypeDesc(meme, pawnXenotype, offset);
+                        }
+                    }
+                }
+
+                if (sb != null && !text.NullOrEmpty())
+                {
+                    sb.AppendInNewLine(" -  " + "AbilityIdeoConvertBreakdownPawnIdeo".Translate(pawn.Named("PAWN")) +
+                                       ": " + text);
+                }
+
+                return result;
+            }
+
+            string MemeAndXenotypeDesc(MemeDef meme, XenotypeDef xenotype, float offset)
+            {
+                if (sb == null)
+                {
+                    return string.Empty;
+                }
+                return "\n   -  " + "XylAbilityIdeoConvertBreakdownMemeVsXenotype".Translate(meme.label.Named("MEME"), xenotype.label.Named("XENOTYPE")).CapitalizeFirst() + ": " + (1f + offset).ToStringPercent();
+            }
+        }
+    }
+}
---
+++ Source_XylRaces/XenotypeDefExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    [UsedImplicitly]
+    public class XenotypeDefExtension : DefModExtension
+    {
+        public List<MemeDef> agreeingMemes;
+        public List<MemeDef> disagreeingMemes;
+    }
+}
