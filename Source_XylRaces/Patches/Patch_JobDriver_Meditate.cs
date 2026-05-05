848a38b1503b071b23dc13d32c9563fb1ee9b621
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-05 06:19:09 -0700

    Bugfix: nixies actually regain psyfocus while meditating now

---
+++ Source_XylRaces/Patches/Patch_JobDriver_Meditate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using TranspilerUtil;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(JobDriver_Meditate))]
+    public static class Patch_JobDriver_Meditate
+    {
+        private static readonly InstructionMatcher Fixup_MeditationTick = new()
+        {
+            Rules =
+            {
+                InstructionMatcher.MakeRedirectRule(
+                    AccessTools.PropertyGetter(typeof(Pawn), nameof(Pawn.HasPsylink)),
+                    AccessTools.Method(typeof(Patch_JobDriver_Meditate), nameof(HasPsylink_Wrapper))
+                )
+            }
+        };
+
+        [Feature(nameof(Genes.Psycast)), HarmonyTranspiler, UsedImplicitly, HarmonyPatch("MeditationTick")]
+        public static IEnumerable<CodeInstruction> MeditationTick_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator, MethodBase method)
+        {
+            var instructionsList = new List<CodeInstruction>(instructions);
+            Fixup_MeditationTick.MatchAndReplace(method, ref instructionsList, generator);
+            return instructionsList;
+        }
+
+        public static bool HasPsylink_Wrapper(Pawn __instance)
+        {
+            return __instance.HasPsylink || __instance.HasActivePsycastGene();
+        }
+    }
+}
