d8b73bf65b2a53558f3fc97ea6872b9539a54641
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-05 20:09:17 -0700

    Begin work on rock toss ability

---
+++ Source_XylRaces/CompAbilityEffect_RockToss.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace XylRacesCore
+{
+    public class CompAbilityEffect_RockToss : CompAbilityEffect_WithDest
+    {
+        public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
+        {
+            // TODO
+        }
+
+        public override bool Valid(LocalTargetInfo target, bool showMessages = false)
+        {
+            if (target.Thing?.def.thingCategories?.Contains(ThingCategoryDefOf.StoneChunks) != true)
+                return false;
+
+            return base.Valid(target, showMessages);
+        }
+
+        public override bool CanHitTarget(LocalTargetInfo target)
+        {
+            if (!CanPlaceSelectedTargetAt(target))
+                return false;
+
+            return base.CanHitTarget(target);
+        }
+    }
+}
