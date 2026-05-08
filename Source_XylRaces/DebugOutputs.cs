482b5562d32ae057555158ce1fd6b3ada29f6d1f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-08 08:02:27 -0700

    Add debug table for gene display order to save my sanity

---
+++ Source_XylRaces/DebugOutputs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using LudeonTK;
+using Verse;
+
+namespace XylRacesCore
+{
+    public static class DebugOutputs
+    {
+        [DebugOutput, UsedImplicitly]
+        public static void GeneDisplayOrder()
+        {
+            TableDataGetter<GeneDef>[] columns =
+            [
+                new("defName", x => x.defName),
+                new("label", x => x.LabelCap),
+                new("displayCategory", x => x.displayCategory.defName),
+                new("displayOrderInCategory", x => x.displayOrderInCategory),
+            ];
+            DebugTables.MakeTablesDialog(DefDatabase<GeneDef>.AllDefs.OrderByDescending(x => x.displayCategory.displayPriorityInXenotype).ThenBy(x => x.displayOrderInCategory), columns);
+        }
+    }
+}
