e4f4d637b976149b9b3959d699cc0b9619b3e9db
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-08 03:49:30 -0700

    Add Lithoid gene

---
+++ Source_XylRaces/Genes/ChemicalImmunity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace XylRacesCore.Genes
+{
+    [UsedImplicitly]
+    public class ChemicalImmunity : Gene
+    {
+    }
+}
