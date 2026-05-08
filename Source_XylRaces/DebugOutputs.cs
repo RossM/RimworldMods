315581a0203f842b89f11c69e3d782112781825b
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-08 14:08:38 -0700

    Remove unused usings

--- Source_XylRaces/DebugOutputs.cs
+++ Source_XylRaces/DebugOutputs.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using JetBrains.Annotations;
 using LudeonTK;
 using RimWorld;
-using UnityEngine;
 using Verse;
 
 namespace XylRacesCore
--- Source_XylRaces/Patches/Patch_XenotypeSet.cs
+++ Source_XylRaces/Patches/Patch_XenotypeSet.cs
@@ -1,15 +1,10 @@
 ﻿using HarmonyLib;
 using JetBrains.Annotations;
 using RimWorld;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using TranspilerUtil;
-using XylRacesCore.Genes;
 
 namespace XylRacesCore.Patches
 {
