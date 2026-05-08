adf41fc323006ae42de1105db1397c7959c86621
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-08 09:45:27 -0700

    Add debug table for faction xenotypes

--- Source_XylRaces/DebugOutputs.cs
+++ Source_XylRaces/DebugOutputs.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
 using LudeonTK;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace XylRacesCore
@@ -22,6 +24,45 @@ namespace XylRacesCore
                 new("displayOrderInCategory", geneDef => geneDef.displayOrderInCategory),
             ];
             DebugTables.MakeTablesDialog(DefDatabase<GeneDef>.AllDefs.OrderByDescending(geneDef => geneDef.displayCategory.displayPriorityInXenotype).ThenBy(geneDef => geneDef.displayOrderInCategory), columns);
+        }
+
+        [DebugOutput, UsedImplicitly]
+        public static void FactionXenotypes()
+        {
+            List<TableDataGetter<FactionDef>> columns =
+            [
+                new("defName", geneDef => geneDef.defName),
+                new("label", geneDef => geneDef.LabelCap),
+            ];
+            foreach (var xenotypeDef in DefDatabase<XenotypeDef>.AllDefs)
+            {
+                var xenotypeDefCaptured = xenotypeDef;
+
+                columns.Add(new(xenotypeDefCaptured.defName, factionDef =>
+                {
+                    float xenotypeChance = GetXenotypeChance(factionDef, xenotypeDefCaptured);
+                    xenotypeChance = Mathf.Min(xenotypeChance, 1.0f);
+                    return xenotypeChance > 0 ? xenotypeChance.ToStringPercent() : "";
+                }));
+            }
+            DebugTables.MakeTablesDialog(DefDatabase<FactionDef>.AllDefs, columns.ToArray());
+            return;
+
+            static float GetXenotypeChance(FactionDef factionDef, XenotypeDef xenotypeDef)
+            {
+                XenotypeSet xenotypeSet = factionDef.xenotypeSet;
+                if (xenotypeDef == XenotypeSetWithDefault.GetDefaultXenotype(xenotypeSet))
+                    return factionDef.BaselinerChance;
+                if (xenotypeSet == null)
+                    return 0f;
+                for (int i = 0; i < xenotypeSet.Count; i++)
+                {
+                    if (xenotypeDef == xenotypeSet[i].xenotype)
+                        return xenotypeSet[i].chance;
+                }
+
+                return 0f;
+            }
         }
     }
 }
