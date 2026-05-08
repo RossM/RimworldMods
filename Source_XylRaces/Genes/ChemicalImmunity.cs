e38474733e46b99d06ee2ffe7197ab20e39b48c5
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-08 14:00:39 -0700

    Add some code to remove invalid hediffs when gene-allowed drug set changes

--- Source_XylRaces/Genes/ChemicalImmunity.cs
+++ Source_XylRaces/Genes/ChemicalImmunity.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
+using RimWorld;
 using Verse;
 
 namespace XylRacesCore.Genes
@@ -11,5 +12,37 @@ namespace XylRacesCore.Genes
     [UsedImplicitly]
     public class ChemicalImmunity : Gene
     {
+        public void RemoveInvalidHediffs()
+        {
+            HashSet<HediffDef> hediffDefsToRemove = new();
+
+            foreach (var chemicalDef in DefDatabase<ChemicalDef>.AllDefs)
+            {
+                if (!pawn.ChemicalIsAllowedByGenes(chemicalDef))
+                {
+                    if (chemicalDef.toleranceHediff != null)
+                        hediffDefsToRemove.Add(chemicalDef.toleranceHediff);
+                    if (chemicalDef.addictionHediff != null)
+                        hediffDefsToRemove.Add(chemicalDef.addictionHediff);
+                }
+            }
+
+            var hediffs = new List<Hediff>(pawn.health.hediffSet.hediffs);
+            foreach (var hediff in hediffs)
+            {
+                if (hediffDefsToRemove.Contains(hediff.def))
+                    pawn.health.RemoveHediff(hediff);
+            }
+        }
+
+        public override void PostAdd()
+        {
+            RemoveInvalidHediffs();
+        }
+
+        public override void PostRemove()
+        {
+            RemoveInvalidHediffs();
+        }
     }
 }
