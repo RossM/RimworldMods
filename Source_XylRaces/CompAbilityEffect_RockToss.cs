eae176ceab151a0a12726c242c839e2a8e4255a4
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-12 00:48:01 -0700

    Add rock toss XML, get targeting working

--- Source_XylRaces/CompAbilityEffect_RockToss.cs
+++ Source_XylRaces/CompAbilityEffect_RockToss.cs
@@ -1,10 +1,25 @@
-﻿using RimWorld;
+﻿using JetBrains.Annotations;
+using RimWorld;
+using UnityEngine;
 using Verse;
 
 namespace XylRacesCore
 {
-    public class CompAbilityEffect_RockToss : CompAbilityEffect_WithDest
+    [UsedImplicitly]
+    public class CompProperties_AbilityRockToss : CompProperties_EffectWithDest
     {
+        public float minRange;
+
+        public CompProperties_AbilityRockToss()
+        {
+            compClass = typeof(CompAbilityEffect_RockToss);
+        }
+    }
+
+    public class CompAbilityEffect_RockToss : CompAbilityEffect_WithDest, ITargetingSource
+    {
+        public new CompProperties_AbilityRockToss Props => (CompProperties_AbilityRockToss)props;
+     
         public override void Apply(LocalTargetInfo target, LocalTargetInfo dest)
         {
             // TODO
@@ -22,8 +37,22 @@ namespace XylRacesCore
         {
             if (!CanPlaceSelectedTargetAt(target))
                 return false;
+            if (target.Cell.DistanceTo(selectedTarget.Cell) < Props.minRange)
+                return false;
 
             return base.CanHitTarget(target);
+        }
+
+        void ITargetingSource.DrawHighlight(LocalTargetInfo target)
+        {
+            if (Props.range > 0f)
+            {
+                GenDraw.DrawRadiusRing(selectedTarget.Cell, Props.range, Color.white, c => c.DistanceTo(selectedTarget.Cell) >= Props.minRange);
+            }
+            if (target.IsValid)
+            {
+                GenDraw.DrawTargetHighlight(target);
+            }
         }
     }
 }
