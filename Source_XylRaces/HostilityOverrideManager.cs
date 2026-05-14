8052863b8ce2e6bd9aa26990e7c11b4943509fbd
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-14 02:32:55 -0700

    Fix HostilityManager save/load

--- Source_XylRaces/HostilityOverrideManager.cs
+++ Source_XylRaces/HostilityOverrideManager.cs
@@ -20,8 +20,37 @@ namespace XylRacesCore
 
         public override void ExposeData()
         {
-            Scribe_Collections.Look(ref activeOverrides, nameof(activeOverrides), LookMode.Reference);
+            Look(ref activeOverrides, nameof(activeOverrides), LookMode.Reference);
+
             Scribe_Collections.Look(ref lastHostileActionTick, nameof(lastHostileActionTick), keyLookMode: LookMode.Deep, valueLookMode: LookMode.Value);
+        }
+
+        private static void Look<T1, T2>(ref HashSet<(T1, T2)> valueTuples, string label, LookMode lookMode)
+        {
+            List<T1> listFirst = new();
+            List<T2> listSecond = new();
+            if (Scribe.mode == LoadSaveMode.Saving)
+            {
+                foreach (var pair in valueTuples)
+                {
+                    listFirst.Add(pair.Item1);
+                    listSecond.Add(pair.Item2);
+                }
+            }
+            
+            Scribe_Collections.Look(ref listFirst, label + "_first", lookMode);
+            Scribe_Collections.Look(ref listSecond, label + "_second", lookMode);
+
+            if ((lookMode == LookMode.Reference && Scribe.mode == LoadSaveMode.ResolvingCrossRefs) ||
+                (lookMode != LookMode.Reference && Scribe.mode == LoadSaveMode.LoadingVars))
+            {
+                valueTuples = [];
+                if (listFirst != null && listSecond != null)
+                {
+                    foreach (var pair in listFirst.Zip(listSecond, (f, s) => (f, s)))
+                        valueTuples.Add(pair);
+                }
+            }
         }
 
         public static HostilityOverrideManager GetManager(Map map)
