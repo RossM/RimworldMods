81ce49066f8de52f6350cb0be86c73966cdf0404
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-14 07:58:01 -0700

    Suppress "need recreation variety" alert when all colonists are joyless

---
+++ Source_XylRaces/Patches/Patch_Alert_NeedJoySources.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace XylRacesCore.Patches
+{
+    [HarmonyPatch(typeof(Alert_NeedJoySources))]
+    public static class Patch_Alert_NeedJoySources
+    {
+        [Feature("Joyless"), HarmonyPrefix, UsedImplicitly, HarmonyPatch("NeedJoySource")]
+        public static bool NeedJoySource_Prefix(Map map, bool __result)
+        {
+            __result = false;
+
+            // Check if any pawns need joy
+            if (!map.mapPawns.FreeColonists.Any(pawn => pawn.needs.joy != null))
+                return false;
+
+            // Continue to regular checks
+            return true;
+        }
+    }
+}
