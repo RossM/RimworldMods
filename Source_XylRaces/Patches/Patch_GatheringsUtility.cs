5c82b1ab847757a3ddb99e0ab1fb84539098fa5c
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-18 01:49:13 -0700

    Prevent joyless pawns from starting or attending parties

---
+++ Source_XylRaces/Patches/Patch_GatheringsUtility.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace XylXenos.Patches
+{
+    [HarmonyPatch(typeof(GatheringsUtility))]
+    public static class Patch_GatheringsUtility
+    {
+        [Feature("Joyless")]
+        [HarmonyPrefix]
+        [UsedImplicitly]
+        [HarmonyPatch(nameof(GatheringsUtility.ShouldPawnKeepGathering))]
+        public static bool ShouldPawnKeepGathering(Pawn p, GatheringDef gatheringDef, ref bool __result)
+        {
+            __result = false;
+            if (gatheringDef.respectTimetable && p.needs.joy == null)
+                return false;
+            return true;
+        }
+    }
+}
