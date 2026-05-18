b85b017ab445b739563feaedbd1e8749c9cbce02
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-18 00:39:20 -0700

    Gene UI tweaks

--- Source_XylRaces/Genes/GeneDefExtension_UIFilter.cs
+++ Source_XylRaces/Genes/GeneDefExtension_UIFilter.cs
@@ -2,11 +2,16 @@
 {
     public class GeneDefExtension_UIFilter : GeneDefExtension
     {
+        public bool alwaysHide = false;
         public bool? inheritable;
 
         public bool ShouldBeVisible(bool isInheritable)
         {
-            return inheritable == null || inheritable == isInheritable;
+            if (alwaysHide)
+                return false;
+            if (inheritable != null && inheritable != isInheritable)
+                return false;
+            return true;
         }
     }
 }
