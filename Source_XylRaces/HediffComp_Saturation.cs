7bba0aefeb6f3a5f319b095558c1565bb2a59c3f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-18 21:26:25 -0700

    Add atlasite

---
+++ Source_XylRaces/HediffComp_Saturation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Verse;
+
+namespace XylXenos
+{
+    [UsedImplicitly]
+    public class HediffCompProperties_Saturation : HediffCompProperties
+    {
+        public HediffDef sourceHediff;
+        public float severityLossPerDay;
+        public float severityGainPerDay;
+
+        public HediffCompProperties_Saturation()
+        {
+            compClass = typeof(HediffComp_Saturation);
+        }
+    }
+
+    public class HediffComp_Saturation : HediffComp_SeverityModifierBase
+    {
+        public HediffCompProperties_Saturation Props => (HediffCompProperties_Saturation)props;
+
+        public override float SeverityChangePerDay()
+        {
+            return Pawn.health.hediffSet.HasHediff(Props.sourceHediff) ? Props.severityGainPerDay : Props.severityLossPerDay;
+        }
+
+        public override string CompLabelInBracketsExtra => (parent.Severity / parent.def.maxSeverity).ToStringPercent();
+    }
+}
