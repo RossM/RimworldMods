ea49294e7366f6e38c063631c09753b0d1f71000
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-22 09:35:57 -0700

    Code cleanup

--- Source_XylRaces/Genes/GeneDefExtension_JoyGivers.cs
+++ Source_XylRaces/Genes/GeneDefExtension_JoyGivers.cs
@@ -9,19 +9,19 @@ namespace XylXenos.Genes
     [UsedImplicitly]
     public class GeneDefExtension_JoyGivers : GeneDefExtension
     {
-        public class JoyGiverFactor
-        {
-            public JoyGiverDef joyGiver;
-            public float factor = 1.0f;
-
-            [UsedImplicitly]
-            public void LoadDataFromXmlCustom(XmlNode xmlRoot)
-            {
-                DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "joyGiver", xmlRoot.Name);
-                factor = ParseHelper.FromString<float>(xmlRoot.FirstChild.Value);
-            }
-        }
-
         public List<JoyGiverFactor> joyGiverChanceFactors;
+    }
+
+    public class JoyGiverFactor
+    {
+        public JoyGiverDef joyGiver;
+        public float factor = 1.0f;
+
+        [UsedImplicitly]
+        public void LoadDataFromXmlCustom(XmlNode xmlRoot)
+        {
+            DirectXmlCrossRefLoader.RegisterObjectWantsCrossRef(this, "joyGiver", xmlRoot.Name);
+            factor = ParseHelper.FromString<float>(xmlRoot.FirstChild.Value);
+        }
     }
 }
