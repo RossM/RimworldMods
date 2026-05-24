f83d30f16c1c13640ac2e3f928b746a78252e92b
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-24 09:26:55 -0700

    Add check for ideology

--- Source_XylIdeoTweaks/Patch_OutfitDatabase.cs
+++ Source_XylIdeoTweaks/Patch_OutfitDatabase.cs
@@ -34,30 +34,34 @@ namespace Source_XylIdeoTweaks
                 }
             }
 
-            var outfitSlavePermissive = __instance.MakeNewOutfit();
-            outfitSlavePermissive.label = "Slave (permissive)";
-            outfitSlavePermissive.filter.SetDisallowAll();
-            outfitSlavePermissive.filter.SetAllow(SpecialThingFilterDefOf.AllowDeadmansApparel, allow: false);
-            foreach (ThingDef def in DefDatabase<ThingDef>.AllDefs)
+            if (ModsConfig.IdeologyActive)
             {
-                if (def.apparel == null)
-                    continue;
-
-                if (def.apparel.defaultOutfitTags.NotNullAndContains("Slave"))
+                var outfitSlavePermissive = __instance.MakeNewOutfit();
+                outfitSlavePermissive.label = "Slave (permissive)";
+                outfitSlavePermissive.filter.SetDisallowAll();
+                outfitSlavePermissive.filter.SetAllow(SpecialThingFilterDefOf.AllowDeadmansApparel, allow: false);
+                foreach (ThingDef def in DefDatabase<ThingDef>.AllDefs)
                 {
+                    if (def.apparel == null)
+                        continue;
+
+                    if (def.apparel.defaultOutfitTags.NotNullAndContains("Slave"))
+                    {
+                        outfitSlavePermissive.filter.SetAllow(def, allow: true);
+                        continue;
+                    }
+
+                    if (def.equippedStatOffsets != null &&
+                        def.equippedStatOffsets.Any(modifier => modifier.stat == Defs.SlaveSuppressionOffset && modifier.value < 0))
+                        continue;
+                    if (def.apparel.bodyPartGroups.Contains(BodyPartGroupDefOf.Torso) &&
+                        def.apparel.layers.Contains(ApparelLayerDefOf.Shell))
+                        continue;
+                    if (def.apparel.bodyPartGroups.Contains(Defs.Neck) && def.apparel.layers.Contains(ApparelLayerDefOf.Overhead))
+                        continue;
+
                     outfitSlavePermissive.filter.SetAllow(def, allow: true);
-                    continue;
                 }
-
-                if (def.equippedStatOffsets != null &&
-                    def.equippedStatOffsets.Any(modifier => modifier.stat == Defs.SlaveSuppressionOffset && modifier.value < 0))
-                    continue;
-                if (def.apparel.bodyPartGroups.Contains(BodyPartGroupDefOf.Torso) && def.apparel.layers.Contains(ApparelLayerDefOf.Shell))
-                    continue;
-                if (def.apparel.bodyPartGroups.Contains(Defs.Neck) && def.apparel.layers.Contains(ApparelLayerDefOf.Overhead))
-                    continue;
-
-                outfitSlavePermissive.filter.SetAllow(def, allow: true);
             }
         }
     }
