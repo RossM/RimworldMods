aacded6286ae595444a03f63978116b7e278ff28
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-24 13:24:30 -0700

    Use postfix

--- Source_XylRaces/Patches/Patch_FactionDef.cs
+++ Source_XylRaces/Patches/Patch_FactionDef.cs
@@ -8,12 +8,11 @@ namespace XylXenos.Patches
     public static class Patch_FactionDef
     {
         [Feature(typeof(XenotypeSetWithDefault))]
-        [InfixPrefix(typeof(XenotypeDefOf), nameof(XenotypeDefOf.Baseliner))]
+        [InfixPostfix(typeof(XenotypeDefOf), nameof(XenotypeDefOf.Baseliner))]
         [InfixPatch(nameof(FactionDef.Description))]
-        public static bool XenotypeDefOf_Baseliner_Prefix(FactionDef __caller, out XenotypeDef __result)
+        public static void XenotypeDefOf_Baseliner_Postfix(FactionDef __caller, ref XenotypeDef __result)
         {
             __result = __caller.xenotypeSet.GetDefaultXenotype();
-            return false;
         }
     }
 }
