c55008345825e7c32c851c3397d086dc6faa84a4
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-26 07:46:30 -0700

    Allow tattoos to draw on top of scaleskin

---
+++ Source_XylRaces/Patches/Patch_PawnRenderNode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+
+namespace XylXenos.Patches
+{
+    [HarmonyPatch(typeof(PawnRenderNode))]
+    public static class Patch_PawnRenderNode
+    {
+        // This allows tattoos to draw properly on top of scaleskin
+        [Feature("TODO")]
+        [HarmonyPostfix]
+        [HarmonyPatch(nameof(PawnRenderNode.AddChildren))]
+        public static void AddChildren_Postfix(PawnRenderNode __instance)
+        {
+            Array.Sort(__instance.children, (a, b) => (int)Mathf.Sign(a.Props.baseLayer - b.Props.baseLayer));
+        }
+    }
+}
