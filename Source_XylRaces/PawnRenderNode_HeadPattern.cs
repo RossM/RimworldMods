cc4dc68e906fbee5d7e5f31a2638140dcd4bb561
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-30 11:06:32 -0700

    Use skin overlays for osteoliths rather than full custom head/body

---
+++ Source_XylRaces/PawnRenderNode_HeadPattern.cs
@@ -0,0 +1,20 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class PawnRenderNode_HeadPattern(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+    : PawnRenderNode_Head(pawn, props, tree)
+{
+    //public override GraphicMeshSet MeshSetFor(Pawn pawn)
+    //{
+    //    return HumanlikeMeshPoolUtility.GetHumanlikeHairSetForPawn(pawn);
+    //}
+
+    public override Graphic GraphicFor(Pawn pawn)
+    {
+        if (!pawn.health.hediffSet.HasHead)
+            return null;
+
+        string maskPath = pawn.story.headType.graphicPath;
+        return GraphicDatabase.Get<Graphic_Multi>(TexPathFor(pawn), ShaderDatabase.CutoutSkinOverlay, Vector2.one, ColorFor(pawn), Color.white, null, maskPath);
+    }
+}
