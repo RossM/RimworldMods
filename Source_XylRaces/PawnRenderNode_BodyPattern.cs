18d5cb828179dca79e465a35840df8c05521a3de
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-30 07:38:27 -0700

    Add ribs cosmetic gene (south art only)

---
+++ Source_XylRaces/PawnRenderNode_BodyPattern.cs
@@ -0,0 +1,12 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class PawnRenderNode_BodyPattern(Pawn pawn, PawnRenderNodeProperties props, PawnRenderTree tree)
+    : PawnRenderNode_Body(pawn, props, tree)
+{
+    public override Graphic GraphicFor(Pawn pawn)
+    {
+        string maskPath = pawn.story.bodyType.bodyNakedGraphicPath;
+        return GraphicDatabase.Get<Graphic_Multi>(TexPathFor(pawn), ShaderDatabase.CutoutSkinOverlay, Vector2.one, ColorFor(pawn), Color.white, null, maskPath);
+    }
+}
