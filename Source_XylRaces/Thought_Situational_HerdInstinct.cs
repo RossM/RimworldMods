878edd2145d18781bac403f8110220ee24840971
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-31 05:45:59 -0700

    Simplify curve

--- Source_XylRaces/Thought_Situational_HerdInstinct.cs
+++ Source_XylRaces/Thought_Situational_HerdInstinct.cs
@@ -9,19 +9,10 @@ public class Thought_Situational_HerdInstinct : Thought_Situational
 
     private static readonly SimpleCurve MoodOffsetCurveFromPopulation =
     [
-        new CurvePoint(1f, -15f),
-        new CurvePoint(2f, -12f),
-        new CurvePoint(3f, -9f),
-        new CurvePoint(4f, -6f),
-        new CurvePoint(5f, -3f),
-        new CurvePoint(6f, 0f),
-        new CurvePoint(12f, 0f),
-        new CurvePoint(13f, 1f),
-        new CurvePoint(14f, 2f),
-        new CurvePoint(15f, 3f),
-        new CurvePoint(16f, 4f),
-        new CurvePoint(17f, 5f),
-        new CurvePoint(18f, 6f),
+        new(1f, -15f),
+        new(6f, 0f),
+        new(12f, 0f),
+        new(18f, 6f),
     ];
 
     public override float MoodOffset()
