d0d0375abd6c6527d2c8daf8c6ca2b8e9b191ca9
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-05-31 02:49:54 -0700

    Fix patches that were not applying

--- Source_XylRaces/Patches/PatchPsycast.cs
+++ Source_XylRaces/Patches/PatchPsycast.cs
@@ -1,10 +1,11 @@
 namespace XylXenos.Patches;
 
+[HarmonyPatch]
 public static class PatchPsycast
 {
     [Feature(nameof(DefModExtension_Gene.hasPsycast))]
     [InfixPostfix(typeof(PawnUtility), nameof(PawnUtility.GetPsylinkLevel))]
-    [InfixPatch("DisabledCheck")]
+    [InfixPatch(typeof(Command_Psycast), "DisabledCheck")]
     public static void GetPsylinkLevel_Postfix(Command_Psycast __caller, Pawn pawn, ref int __result)
     {
         __result = Math.Max(__result, pawn.GetGeneticPsylinkLevelFor(__caller.Ability.def));
@@ -12,8 +13,8 @@ public static class PatchPsycast
 
     [Feature(nameof(DefModExtension_Gene.hasPsycast))]
     [InfixPostfix(typeof(PawnUtility), nameof(PawnUtility.GetPsylinkLevel))]
-    [InfixPatch("GizmoDisabled")]
-    [InfixPatch("CanCast")]
+    [InfixPatch(typeof(Psycast), "GizmoDisabled")]
+    [InfixPatch(typeof(Psycast), "CanCast")]
     public static void GetPsylinkLevel_Postfix(Psycast __caller, Pawn pawn, ref int __result)
     {
         __result = Math.Max(__result, pawn.GetGeneticPsylinkLevelFor(__caller.def));
