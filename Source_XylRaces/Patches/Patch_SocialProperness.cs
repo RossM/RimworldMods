a280324fa7897bb797eff8b587e6a287f30bdc92
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-05 02:05:40 -0700

    Remove unused parameters

--- Source_XylRaces/Patches/Patch_SocialProperness.cs
+++ Source_XylRaces/Patches/Patch_SocialProperness.cs
@@ -6,7 +6,7 @@ public class Patch_SocialProperness
     [Feature(typeof(Gene_Hyperlactation))]
     [InfixPostfix(typeof(GridsUtility), nameof(GridsUtility.IsInPrisonCell))]
     [InfixPatch(nameof(SocialProperness.IsSociallyProper), [typeof(Thing), typeof(Pawn), typeof(bool), typeof(bool)])]
-    public static void IsInPrisonCell_Postfix(Thing t, IntVec3 c, Map map, ref bool __result)
+    public static void IsInPrisonCell_Postfix(Thing t, ref bool __result)
     {
         if (PatchHelpers.HyperlactatingPrisonerInRoomCanProduce(t.GetRoom(), t.def))
             __result = false;
