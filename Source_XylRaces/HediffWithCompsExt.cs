2f34c0002b804ee80a80ee90e9260d1984f77713
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-08 13:41:30 -0700

    Formatted tooltips for love euphoria hediffs

--- Source_XylRaces/HediffWithCompsExt.cs
+++ Source_XylRaces/HediffWithCompsExt.cs
@@ -25,4 +25,42 @@ public class HediffWithCompsExt : HediffWithComps
         base.ExposeData();
         Scribe_References.Look(ref sourcePawn, nameof(sourcePawn));
     }
+
+    private static readonly StringBuilder tipSb = new();
+
+    public override string GetTooltip(Pawn pawn, bool showHediffsDebugInfo)
+    {
+        tipSb.Clear();
+        HediffStage curStage = CurStage;
+        if (!LabelCap.NullOrEmpty())
+            tipSb.AppendTagged(LabelCap.Colorize(ColoredText.TipSectionTitleColor));
+        string severityLabel = SeverityLabel;
+        if (!severityLabel.NullOrEmpty())
+            tipSb.Append(": ").Append(severityLabel);
+        tipSb.AppendLine();
+        if (!def.overrideTooltip.NullOrEmpty())
+            tipSb.AppendLine().AppendLineTagged(def.overrideTooltip.Formatted(pawn.Named("PAWN"), sourcePawn.Named("PARTNER")));
+        else if (curStage != null && !curStage.overrideTooltip.NullOrEmpty())
+        {
+            tipSb.AppendLine().AppendLineTagged(curStage.overrideTooltip.Formatted(pawn.Named("PAWN"), sourcePawn.Named("PARTNER")));
+        }
+        else
+        {
+            string description = Description;
+            if (!description.NullOrEmpty())
+                tipSb.AppendLine().AppendLine(description);
+        }
+        if (!def.extraTooltip.NullOrEmpty())
+            tipSb.AppendLine().AppendLineTagged(def.extraTooltip.Formatted(pawn.Named("PAWN"), sourcePawn.Named("PARTNER")));
+        if (curStage != null && !curStage.extraTooltip.NullOrEmpty())
+            tipSb.AppendLine().AppendLineTagged(curStage.extraTooltip.Formatted(pawn.Named("PAWN"), sourcePawn.Named("PARTNER")));
+        string tipStringExtra = TipStringExtra;
+        if (!tipStringExtra.NullOrEmpty())
+            tipSb.AppendLine().AppendLine(tipStringExtra.TrimEndNewlines());
+        if (HealthCardUtility.GetCombatLogInfo(Gen.YieldSingle(this), out var taggedString, out var _) && !taggedString.NullOrEmpty())
+            tipSb.AppendLine().AppendLineTagged(("Cause".Translate() + ": " + taggedString).Colorize(ColoredText.SubtleGrayColor));
+        if (showHediffsDebugInfo && !DebugString().NullOrEmpty() && !DebugString().NullOrEmpty())
+            tipSb.AppendLine().AppendLine(DebugString().TrimEndNewlines());
+        return tipSb.ToString().TrimEnd();
+    }
 }
