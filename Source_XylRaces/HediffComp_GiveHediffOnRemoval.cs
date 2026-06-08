8473da58c01902aad30b17b54a4af9455c0a17bc
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-08 03:48:47 -0700

    Gut HediffComp_GiveHediffExt, now it's HediffCompProperties_GiveHediffOnRemoval and does much less

---
+++ Source_XylRaces/HediffComp_GiveHediffOnRemoval.cs
@@ -0,0 +1,37 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class HediffCompProperties_GiveHediffOnRemoval : HediffCompProperties
+{
+    public HediffDef hediff;
+
+    public bool inheritSeverity;
+
+    public HediffCompProperties_GiveHediffOnRemoval()
+    {
+        compClass = typeof(HediffComp_GiveHediffOnRemoval);
+    }
+}
+
+public class HediffComp_GiveHediffOnRemoval : HediffComp
+{
+    public HediffCompProperties_GiveHediffOnRemoval Props => (HediffCompProperties_GiveHediffOnRemoval)props;
+
+    public override void CompPostPostRemoved()
+    {
+        base.CompPostPostRemoved();
+
+        ApplyTo(parent.pawn);
+    }
+
+    public bool ApplyTo(Pawn pawn)
+    {
+        Hediff hediff2 = HediffMaker.MakeHediff(partRecord: parent.Part, def: Props.hediff, pawn: pawn);
+
+        if (Props.inheritSeverity)
+            hediff2.Severity = parent.Severity;
+
+        pawn.health.AddHediff(hediff2);
+        return true;
+    }
+}
