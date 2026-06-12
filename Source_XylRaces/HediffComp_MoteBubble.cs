a938c88713f36474326319022ada5726c031fd6e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-12 12:20:19 -0700

    Fix seeing red thought bubble

---
+++ Source_XylRaces/HediffComp_MoteBubble.cs
@@ -0,0 +1,35 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class HediffCompProperties_MoteBubble : HediffCompProperties
+{
+    public string iconPath;
+
+    public HediffCompProperties_MoteBubble()
+    {
+        compClass = typeof(HediffComp_MoteBubble);
+    }
+}
+
+public class HediffComp_MoteBubble : HediffComp
+{
+    public HediffCompProperties_MoteBubble Props => (HediffCompProperties_MoteBubble)props;
+
+    public MoteBubble mote;
+
+    public override void CompPostPostAdd(DamageInfo? dinfo)
+    {
+        mote = MoteMaker.MakeThoughtBubble(parent.pawn, Props.iconPath, maintain: true);
+    }
+
+    public override void CompPostPostRemoved()
+    {
+        if (!mote.Destroyed)
+            mote.Destroy();
+    }
+
+    public override void CompPostTick(ref float severityAdjustment)
+    {
+        mote.Maintain();
+    }
+}
--- Source_XylRaces/MentalState_SeeingRed.cs
+++ Source_XylRaces/MentalState_SeeingRed.cs
@@ -1,22 +1,8 @@
 ﻿namespace XylXenos;
 
-public class DefModExtension_MentalState_SeeingRed : DefModExtension
-{
-    public string iconPath;
-}
-
 [UsedFromXml]
 public class MentalState_SeeingRed : MentalState
 {
-    public DefModExtension_MentalState_SeeingRed DefExt => def.GetModExtension<DefModExtension_MentalState_SeeingRed>();
-    private MoteBubble mote;
-
-    public override void ExposeData()
-    {
-        base.ExposeData();
-        Scribe_References.Look(ref mote, nameof(mote));
-    }
-
     public override bool ForceHostileTo(Thing t)
     {
         return pawn.HasActiveGeneOfType<Gene_SeeingRed>(g => g.ForceHostility(t));
@@ -25,39 +11,5 @@ public class MentalState_SeeingRed : MentalState
     public override RandomSocialMode SocialModeMax()
     {
         return RandomSocialMode.Off;
-    }
-
-    public override void PostStart(string reason)
-    {
-        base.PostStart(reason);
-
-        if (!DefExt.iconPath.NullOrEmpty())
-            mote = MakeThoughtBubble(pawn, DefExt.iconPath);
-    }
-
-    public static MoteBubble MakeThoughtBubble(Pawn pawn, string iconPath)
-    {
-        MoteMaker.ExistingMoteBubbleOn(pawn)?.Destroy();
-        MoteBubble obj = (MoteBubble)ThingMaker.MakeThing(DefOf.XylMote_ForceJobMentalState);
-        obj.SetupMoteBubble(ContentFinder<Texture2D>.Get(iconPath), null);
-        obj.Attach(pawn);
-        GenSpawn.Spawn(obj, pawn.Position, pawn.Map);
-        return obj;
-    }
-
-    public override void MentalStateTick(int delta)
-    {
-        base.MentalStateTick(delta);
-
-        if (mote?.Destroyed == false)
-            mote.Maintain();
-    }
-
-    public override void PostEnd()
-    {
-        base.PostEnd();
-
-        if (mote?.Destroyed == false)
-            mote.Destroy();
     }
 }
