3f1ec49c008e070fc0a0abcb5786ec5ac0058bb8
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-12 19:21:25 -0700

    Use prefix to fix potential issue with outputting the reason

--- Source_XylRaces/Patches/Patch_JoyUtility.cs
+++ Source_XylRaces/Patches/Patch_JoyUtility.cs
@@ -4,13 +4,20 @@
 public static class Patch_JoyUtility
 {
     [Feature(typeof(Need_Wetness))]
-    [InfixPostfix(typeof(JoyUtility), nameof(JoyUtility.EnjoyableOutsideNow), [typeof(Map), typeof(StringBuilder)])]
+    [InfixPrefix(typeof(JoyUtility), nameof(JoyUtility.EnjoyableOutsideNow), [typeof(Map), typeof(StringBuilder)])]
     [InfixPatch(nameof(JoyUtility.EnjoyableOutsideNow), [typeof(Pawn), typeof(StringBuilder)])]
-    public static void EnjoyableOutsideNow_Postfix(Pawn pawn, Map map, ref bool __result)
+    public static bool EnjoyableOutsideNow_Prefix(Pawn pawn, Map map, StringBuilder outFailReason, out bool __result)
     {
+        __result = false;
+
         if (pawn.needs.TryGetNeed<Need_Wetness>() != null)
         {
             __result = map.gameConditionManager.AllowEnjoyableOutsideNow(map, out var reason);
+            if (__result == false)
+                outFailReason?.Append(reason.label);
+            return false;
         }
+
+        return true;
     }
 }
