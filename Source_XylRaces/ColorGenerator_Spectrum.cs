eec0904f416169e54dcbf42ce8845571678a13ff
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-14 05:12:13 -0700

    Use a color spectrum rather than fixed colors for siren starting clothes

---
+++ Source_XylRaces/ColorGenerator_Spectrum.cs
@@ -0,0 +1,18 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class ColorGenerator_Spectrum : ColorGenerator
+{
+    public override Color ExemplaryColor => colors[0];
+    public List<Color> colors;
+
+    public override Color NewRandomizedColor()
+    {
+        if (colors.Count == 1)
+            return colors[0];
+
+        int index = Rand.Range(0, colors.Count - 2);
+        float fraction = Rand.Value;
+        return Color.Lerp(colors[index], colors[index + 1], fraction);
+    }
+}
