1daa6ea4a952ef0f87a5bf4485cf15bf5431eeac
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-06-26 20:30:03 -0700

    Add comments explaining the style rules

--- Source_Xylib/Analyzer.cs
+++ Source_Xylib/Analyzer.cs
@@ -5,8 +5,8 @@ namespace Xylib;
 public static class Analyzer
 {
     /// <summary>
-    /// This checks the given assembly for Harmony patches annotated with <see cref="HarmonyPatch"/> and checks
-    /// for issues that might indicate a potential bug or maintainability problem.
+    ///     This checks the given assembly for Harmony patches annotated with <see cref="HarmonyPatch" /> and checks
+    ///     for issues that might indicate a potential bug or maintainability problem.
     /// </summary>
     /// <param name="assembly"></param>
     public static void CheckCodingStyle_Patches(Assembly assembly)
@@ -25,21 +25,29 @@ public static class Analyzer
                 var hasInfixPrefix = method.HasAttribute<InfixPrefixAttribute>();
                 var hasInfixPostfix = method.HasAttribute<InfixPostfixAttribute>();
 
+                // A patch class without [HarmonyPatch] won't get processed, so this almost certainly indicates a bug
                 if ((hasPrefix || hasPostfix || hasTranspiler || hasInfixPatch) && !typeHasHarmony)
                     Log.Warning($"{type.FullName}::{method.Name} appears to be a patch but is in a type with no [HarmonyPatch] attribute");
 
                 if (!typeHasHarmony)
                     continue;
 
+                // Putting a [Feature] attribute on each patch helps track which patches do what
                 if ((hasPrefix || hasPostfix || hasTranspiler || hasInfixPatch) && !hasFeature)
                     Log.Warning($"{type.FullName}::{method.Name} is missing a [Feature] attribute");
+
+                // [Feature] is only intended for harmony patches
                 if (!(hasPrefix || hasPostfix || hasTranspiler || hasInfixPatch) && hasFeature)
                     Log.Warning($"{type.FullName}::{method.Name} has [Feature] but no Harmony attribute");
 
+                // Applying [InfixPatch] without [InfixPrefix] or [InfixPostfix], or vice versa, won't do anything, so is probably a bug
                 if (hasInfixPatch != (hasInfixPrefix || hasInfixPostfix))
+                {
                     Log.Warning(
                         $"{type.FullName}::{method.Name} has should have both [InfixPatch] and one of [InfixPrefix] or [InfixPostfix]");
+                }
 
+                // Enforce a naming convention for patch methods. This makes it more obvious at a glance when a patch will run
                 if ((hasPrefix || hasInfixPrefix) && !(method.Name == "Prefix" || method.Name.EndsWith("_Prefix")))
                     Log.Warning($"{type.FullName}::{method.Name} should be named with _Prefix");
                 if ((hasPostfix || hasInfixPostfix) && !(method.Name == "Postfix" || method.Name.EndsWith("_Postfix")))
@@ -51,14 +59,21 @@ public static class Analyzer
                 ParameterInfo resultParameter = parameters.SingleOrDefault(p => p.Name == "__result");
                 if (hasPrefix || hasInfixPrefix)
                 {
+                    // A prefix __result parameter without 'out' might not be initialized, which results in the default
+                    // value being used if the prefix returns false. This is confusing and potentially indicates a bug.
                     if (resultParameter?.IsOut == false)
                         Log.Warning($"{type.FullName}::{method.Name} should use 'out' for __result");
+
+                    // If a prefix patch returns void, it will always go on to the main method, and the value of
+                    // __result won't be used. This almost certainly indicates a bug.
                     if (method.ReturnType.IsVoid() && resultParameter != null)
                         Log.Warning($"{type.FullName}::{method.Name} returns void but uses __result");
                 }
 
                 if (hasPostfix || hasInfixPostfix)
                 {
+                    // Postfix patches taking __result usually want to modify it, which won't work without 'ref',
+                    // so a missing 'ref' modifier potentially indicates a bug.
                     if (resultParameter is { ParameterType.IsByRef: false })
                         Log.Warning($"{type.FullName}::{method.Name} has a non-ref __result");
                 }
