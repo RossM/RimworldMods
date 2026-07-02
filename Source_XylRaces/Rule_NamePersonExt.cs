ac0b2a7c04b0a99bef08c2848464578d4f2d398c
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-02 16:54:10 -0700

    Rework bossaps namer to use normal human names

---
+++ Source_XylRaces/Rule_NamePersonExt.cs
@@ -0,0 +1,30 @@
+﻿using Verse.Grammar;
+
+namespace XylXenos;
+
+[UsedFromXml]
+public class Rule_NamePersonExt : Rule
+{
+    public override float BaseSelectionWeight => 1f;
+    public Gender gender;
+    public PawnNameSlot slot = PawnNameSlot.First;
+
+    public override Rule DeepCopy()
+    {
+        Rule_NamePersonExt copy = (Rule_NamePersonExt)base.DeepCopy();
+        copy.gender = gender;
+        copy.slot = slot;
+        return copy;
+    }
+
+    public override string Generate()
+    {
+        NameBank nameBank = PawnNameDatabaseShuffled.BankOf(PawnNameCategory.HumanStandard);
+        return nameBank.GetName(slot, gender, checkIfAlreadyUsed: false);
+    }
+
+    public override string ToString()
+    {
+        return keyword + "->(personname)";
+    }
+}
