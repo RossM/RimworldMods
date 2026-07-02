3ba180c165e12a0754d0a33e09176dbfd156e39f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-02 04:53:33 -0700

    Display saturtion stats on atlasite description

--- Source_XylRaces/HediffComp_Saturation.cs
+++ Source_XylRaces/HediffComp_Saturation.cs
@@ -1,15 +1,43 @@
 ﻿namespace XylXenos;
 
 [UsedFromXml]
-public class HediffCompProperties_Saturation : HediffCompProperties
+public class HediffCompProperties_Saturation : HediffCompPropertiesExt
 {
     public HediffDef sourceHediff;
     public float severityLossPerDay;
     public float severityGainPerDay;
 
+    public HediffDef parent;
+
     public HediffCompProperties_Saturation()
     {
         compClass = typeof(HediffComp_Saturation);
+    }
+
+    // ReSharper disable once ParameterHidesMember
+    public override void ResolveReferences(HediffDef parent)
+    {
+        this.parent = parent;
+    }
+
+    public override IEnumerable<StatDrawEntry> SpecialDisplayStats(StatRequest request)
+    {
+        if (severityGainPerDay > 0)
+        {
+            yield return new(StatCategoryDefOf.Drug, "XylSaturationGain".Translate(),
+                "PerDay".Translate(severityGainPerDay.ToStringPercent()),
+                "XylSaturationGainDesc".Translate(), 0);
+            yield return new(StatCategoryDefOf.Drug, "XylDaysToFullSaturation".Translate(),
+                "PeriodDays".Translate((parent.maxSeverity / severityGainPerDay).ToStringDecimalIfSmall()),
+                "XylDaysToFullSaturationDesc".Translate(), 0);
+        }
+
+        if (severityLossPerDay < 0)
+        {
+            yield return new(StatCategoryDefOf.Drug, "XylSaturationLoss".Translate(),
+                "PerDay".Translate(severityLossPerDay.ToStringPercent()),
+                "XylSaturationLossDesc".Translate(), 0);
+        }
     }
 }
 
