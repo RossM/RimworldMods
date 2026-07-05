31ddc47ff0c443abb3d484cb5920cb8dcf7e5850
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-05 15:45:50 -0700

    Prevent gene extractor from extracting endogene-only genes

--- Source_XylRaces/Patches/Patch_GeneDef.cs
+++ Source_XylRaces/Patches/Patch_GeneDef.cs
@@ -1,47 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿using System.Reflection;
 using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace XylXenos.Patches
+namespace XylXenos.Patches;
+
+// GeneDef.GetDescriptionFull doesn't check whether thought stages are null, resulting in a NullReferenceException
+// when processing XylHyperlactation and XylSoreBreasts.
+[HarmonyPatch(typeof(GeneDef))]
+public static class Patch_GeneDef_GetDescriptionFull
 {
-    // GeneDef.GetDescriptionFull doesn't check whether thought stages are null, resulting in a NullReferenceException
-    // when processing XylHyperlactation and XylSoreBreasts.
-    [HarmonyPatch(typeof(GeneDef))]
-    public static class Patch_GeneDef_GetDescriptionFull
+    [Feature(nameof(Config.Feature.Bugfix_Misc))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> Transpiler(
+        IEnumerable<CodeInstruction> instructions,
+        ILGenerator generator,
+        MethodBase method)
     {
-        [HarmonyTargetMethods]
-        public static IEnumerable<MethodInfo> TargetMethods()
-        {
-            var type = AccessTools.TypeByName("Verse.GeneDef");
-            type = type.GetNestedType("<>c", AccessTools.all);
-            foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic))
-            {
-                var parameters = method.GetParameters();
-                if (method.ReturnType == typeof(bool) && parameters.Length == 1 && parameters[0].ParameterType == typeof(ThoughtStage))
-                    yield return method;
-            }
-        }
+        var label = generator.DefineLabel();
 
-        [Feature(nameof(Config.Feature.Bugfix_Misc))]
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> Transpiler(
-            IEnumerable<CodeInstruction> instructions,
-            ILGenerator generator,
-            MethodBase method)
-        {
-            var label = generator.DefineLabel();
+        yield return new(OpCodes.Ldarg_1);
+        yield return new(OpCodes.Brtrue_S, label);
+        yield return new(OpCodes.Ldc_I4_0);
+        yield return new(OpCodes.Ret);
+        yield return new(OpCodes.Nop) { labels = [label] };
+        foreach (var instruction in instructions)
+            yield return instruction;
+    }
 
-            yield return new(OpCodes.Ldarg_1);
-            yield return new(OpCodes.Brtrue_S, label);
-            yield return new(OpCodes.Ldc_I4_0);
-            yield return new(OpCodes.Ret);
-            yield return new(OpCodes.Nop) { labels = [label] };
-            foreach (var instruction in instructions)
-                yield return instruction;
+    [HarmonyTargetMethods]
+    public static IEnumerable<MethodInfo> TargetMethods()
+    {
+        var type = AccessTools.TypeByName("Verse.GeneDef");
+        type = type.GetNestedType("<>c", AccessTools.all);
+        foreach (var method in type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic))
+        {
+            var parameters = method.GetParameters();
+            if (method.ReturnType == typeof(bool) && parameters.Length == 1 && parameters[0].ParameterType == typeof(ThoughtStage))
+                yield return method;
         }
     }
 }
---
+++ Source_Xylib/Patches/Patch_Building_GeneExtractor.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace Xylib.Patches;
+
+[HarmonyPatch(typeof(Building_GeneExtractor))]
+internal static class Patch_Building_GeneExtractor
+{
+    [HarmonyTargetMethod]
+    public static MethodInfo TargetMethod()
+    {
+        var type = AccessTools.TypeByName("RimWorld.Building_GeneExtractor");
+        foreach (var nestedType in type.GetNestedTypes(AccessTools.all))
+        {
+            if (!nestedType.Name.StartsWith("<>c"))
+                continue;
+
+            foreach (var method in nestedType.GetMethods(AccessTools.all))
+            {
+                if (method.Name.StartsWith("<Finish>g__SelectionWeight"))
+                    return method;
+            }
+        }
+
+        return null;
+    }
+
+    [Feature(nameof(DefModExtension_GeneWithComps.geneType))]
+    [HarmonyPostfix]
+    public static void SelectionWeight_Postfix(Gene g, ref float __result)
+    {
+        if (g.def.Extension_GeneWithComps?.geneType is GeneType.Endogene)
+            __result = 0f;
+    }
+}
