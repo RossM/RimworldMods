ec8edfd8fec6c006ef2048be1a775a39d58ed2c7
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-04 23:24:41 -0700

    Code cleanup

--- Source_Xylib/GeneCompProperties_RenderNodeModifiers.cs
+++ Source_Xylib/GeneCompProperties_RenderNodeModifiers.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Xylib;
 
-namespace Xylib
+[UsedFromXml]
+public class GeneCompProperties_RenderNodeModifiers : GeneCompProperties
 {
-    [UsedFromXml]
-    public class GeneCompProperties_RenderNodeModifiers : GeneCompProperties
-    {
-        /// <summary>
-        ///     Modifiers to the scale and offset to specific nodes in the pawn's render tree, used to
-        ///     change the pawn's visual in a different way than just adding additional nodes.
-        /// </summary>
-        [CanBeNull] public List<RenderNodeModifier> renderNodeModifiers;
-    }
+    /// <summary>
+    ///     Modifiers to the scale and offset to specific nodes in the pawn's render tree, used to
+    ///     change the pawn's visual in a different way than just adding additional nodes.
+    /// </summary>
+    [CanBeNull] public List<RenderNodeModifier> renderNodeModifiers;
 }
