12883f3f1f4bcf30fdcd764827b6dcb340853860
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-06 15:14:40 -0700

    Fix label for lovin' withdrawal

--- Source_XylRaces/ThoughtWorker_WithSource.cs
+++ Source_XylRaces/ThoughtWorker_WithSource.cs
@@ -3,6 +3,16 @@
 [UsedFromXml]
 public class ThoughtWorker_WithSource : ThoughtWorker_Hediff
 {
+    public override string PostProcessLabel(Pawn p, string label)
+    {
+        HediffWithCompsExt hediff = p.health.hediffSet.GetFirstHediffOfDef(def.hediff) as HediffWithCompsExt;
+        Pawn sourcePawn = hediff?.sourcePawn;
+
+        label = label.Formatted(p.Named("PAWN"), sourcePawn.Named("SOURCE"));
+
+        return label;
+    }
+
     public override string PostProcessDescription(Pawn p, string description)
     {
         HediffWithCompsExt hediff = p.health.hediffSet.GetFirstHediffOfDef(def.hediff) as HediffWithCompsExt;
