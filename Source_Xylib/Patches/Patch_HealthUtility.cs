a9da705cf6db98aa77659fcfe3f205b2eec7b45b
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-06 01:31:42 -0700

    Resistance should only affect hediff increases

--- Source_Xylib/Patches/Patch_HealthUtility.cs
+++ Source_Xylib/Patches/Patch_HealthUtility.cs
@@ -12,6 +12,9 @@ internal static class Patch_HealthUtility
     [HarmonyPatch(nameof(HealthUtility.AdjustSeverity))]
     public static void AdjustSeverity_Prefix(Pawn pawn, HediffDef hdDef, ref float sevOffset)
     {
+        if (sevOffset < 0)
+            return;
+
         float resistance = PatchHelpers.GetHediffResistance(pawn, hdDef);
         float factor = Mathf.Max(1f - resistance, 0f);
 
