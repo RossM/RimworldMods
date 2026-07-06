82d9282c3ba6034a8bbed68a12e4a889dbeae38a
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-05 23:53:58 -0700

    Test & fix ReflectionHelpers.HasOverridingMethod

--- Source_Xylib/ReflectionHelpers.cs
+++ Source_Xylib/ReflectionHelpers.cs
@@ -1,16 +1,37 @@
-﻿namespace Xylib;
+﻿using System.Reflection;
+
+namespace Xylib;
 
 public static class ReflectionHelpers
 {
+    private const BindingFlags MethodBindingFlags = BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic;
+
     // This is necessary because MethodInfo.GetBaseDefinition is broken for the version of Mono used in Unity
     public static bool HasOverridingMethod(Type childType, Type baseType, string methodName)
     {
+        var baseMethodInfo = baseType.GetMethod(methodName, MethodBindingFlags);
+
         for (Type type = childType; type != null && type != baseType; type = type.BaseType)
         {
-            if (type.GetMethod(methodName) != null)
+            MethodInfo methodInfo = type.GetMethod(methodName, MethodBindingFlags);
+            if (methodInfo != null && methodInfo.GetBaseDefinition() == baseMethodInfo)
                 return true;
         }
 
         return false;
+    }
+
+    [DebugOutput]
+    internal static void GeneCompInfo()
+    {
+        TableDataGetter<Type>[] columns =
+        [
+            new("class", type => type.FullName),
+            new("BaseType", type => type.BaseType?.FullName),
+            new("CompTick", type => HasOverridingMethod(type, typeof(GeneComp), "CompTick")),
+            new("CompTickInterval", type => HasOverridingMethod(type, typeof(GeneComp), "CompTickInterval")),
+        ];
+
+        DebugTables.MakeTablesDialog(typeof(GeneComp).AllSubclassesNonAbstract(), columns);
     }
 }
