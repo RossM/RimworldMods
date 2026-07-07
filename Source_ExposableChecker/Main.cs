0b698a346b359766cdc916d1bb2f28be76575d1a
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-06 17:37:48 -0700

    Run resharper code cleanup

--- Source_ExposableChecker/Main.cs
+++ Source_ExposableChecker/Main.cs
@@ -3,72 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using JetBrains.Annotations;
-using Verse;
 using UnityEngine;
+using Verse;
 using XylDisassembler;
 
-namespace XylExposableChecker
+namespace XylExposableChecker;
+
+[UsedImplicitly]
+[StaticConstructorOnStartup]
+public class Main(ModContentPack content) : Mod(content)
 {
     [UsedImplicitly]
-    [StaticConstructorOnStartup]
-    public class Main(ModContentPack content) : Mod(content)
+    static Main()
     {
-        public static void Check(Type type)
+        HashSet<Assembly> checkedAssemblies = [];
+
+        foreach (Type type in GenTypes.AllTypes)
         {
-            string exposeDataFunc;
-            if (typeof(IExposable).IsAssignableFrom(type))
-                exposeDataFunc = "ExposeData";
-            else if (typeof(ThingComp).IsAssignableFrom(type))
-                exposeDataFunc = "PostExposeData";
-            else if (typeof(HediffComp).IsAssignableFrom(type) || type.Name.EndsWith("Comp"))
-                exposeDataFunc = "CompExposeData";
-            else
-                return;
-
-            var fields = type.GetFields().Where(
-                field => field.GetCustomAttribute<UnsavedAttribute>() == null &&
-                         !field.Attributes.HasFlag(FieldAttributes.Literal) &&
-                         !field.Attributes.HasFlag(FieldAttributes.Static) &&
-                         field.DeclaringType == type).ToList();
-
-            if (fields.Count == 0)
-                return;
-
-            HashSet<FieldInfo> usedFields = [];
-
-            MethodInfo curMethod = type.GetMethod(exposeDataFunc);
-            if (curMethod != null)
+            string assemblyName = type.Assembly.GetName().Name;
+            bool skipAssembly = assemblyName == "Assembly-CSharp";
+            if (!checkedAssemblies.Contains(type.Assembly))
             {
-                var method = Disassembler.Decode(curMethod);
-                usedFields.AddRange(method.Instructions.Select(i => i.Value).OfType<FieldInfo>());
+                Debug.Log(skipAssembly
+                    ? $"IExposable checker: skipping {assemblyName}"
+                    : $"IExposable checker: checking {assemblyName}");
+                checkedAssemblies.Add(type.Assembly);
             }
 
-            foreach (var field in fields.Except(usedFields))
-            {
-                Log.Warning($"Possibly unsaved field: {type.FullName}.{field.Name}. Either save this field in {exposeDataFunc}, mark it [Unsaved], or make it const or readonly.");
-            }
+            if (!skipAssembly)
+                Check(type);
+        }
+    }
+
+    public static void Check(Type type)
+    {
+        string exposeDataFunc;
+        if (typeof(IExposable).IsAssignableFrom(type))
+            exposeDataFunc = "ExposeData";
+        else if (typeof(ThingComp).IsAssignableFrom(type))
+            exposeDataFunc = "PostExposeData";
+        else if (typeof(HediffComp).IsAssignableFrom(type) || type.Name.EndsWith("Comp"))
+            exposeDataFunc = "CompExposeData";
+        else
+            return;
+
+        var fields = type.GetFields().Where(field => field.GetCustomAttribute<UnsavedAttribute>() == null &&
+                                                     !field.Attributes.HasFlag(FieldAttributes.Literal) &&
+                                                     !field.Attributes.HasFlag(FieldAttributes.Static) &&
+                                                     field.DeclaringType == type).ToList();
+
+        if (fields.Count == 0)
+            return;
+
+        HashSet<FieldInfo> usedFields = [];
+
+        MethodInfo curMethod = type.GetMethod(exposeDataFunc);
+        if (curMethod != null)
+        {
+            var method = Disassembler.Decode(curMethod);
+            usedFields.AddRange(method.Instructions.Select(i => i.Value).OfType<FieldInfo>());
         }
 
-        [UsedImplicitly]
-        static Main()
+        foreach (var field in fields.Except(usedFields))
         {
-            HashSet<Assembly> checkedAssemblies = [];
-
-            foreach (Type type in GenTypes.AllTypes)
-            {
-                string assemblyName = type.Assembly.GetName().Name;
-                bool skipAssembly = assemblyName == "Assembly-CSharp";
-                if (!checkedAssemblies.Contains(type.Assembly))
-                {
-                    Debug.Log(skipAssembly
-                        ? $"IExposable checker: skipping {assemblyName}"
-                        : $"IExposable checker: checking {assemblyName}");
-                    checkedAssemblies.Add(type.Assembly);
-                }
-
-                if (!skipAssembly) 
-                    Check(type);
-            }
+            Log.Warning(
+                $"Possibly unsaved field: {type.FullName}.{field.Name}. Either save this field in {exposeDataFunc}, mark it [Unsaved], or make it const or readonly.");
         }
     }
 }
