30b88873157f03d5d1a50aeaf49ab02455dbeb00
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-14 20:24:55 -0700

    Split up Autopatcher.cs

---
+++ Source_Disharmony/Autopatcher.PatchWorker.cs
@@ -0,0 +1,128 @@
+﻿namespace Disharmony;
+
+public static partial class Autopatcher
+{
+    private class PatchWorker(Assembly assembly)
+    {
+        public IEnumerable<MethodInfo> TargetMethods => PatchesByMethod.Keys;
+        private Dictionary<MethodInfo, List<PatchInfo>> PatchesByMethod = new();
+        private Assembly Assembly { get; } = assembly;
+        private Dictionary<MethodInfo, StateBuilder<Type>> StateBuilders { get; } = new();
+        private List<PatchInfo> Patches { get; } = [];
+
+        public void CollectPatches()
+        {
+            foreach (TypeInfo type in Assembly.DefinedTypes)
+            {
+                var harmonyAttribute = (HarmonyPatch?)Attribute.GetCustomAttribute(type, typeof(HarmonyPatch));
+                if (harmonyAttribute == null)
+                    continue;
+
+                foreach (MethodInfo method in type.DeclaredMethods)
+                {
+                    try
+                    {
+                        var infixTargetAttribute
+                            = (PatchTypeAttribute?)Attribute.GetCustomAttribute(method, typeof(InnerPrefixAttribute)) ??
+                              (PatchTypeAttribute?)Attribute.GetCustomAttribute(method, typeof(InnerPostfixAttribute));
+                        var infixPatchAttributes = Attribute.GetCustomAttributes(method, typeof(TargetAttribute))
+                            .Cast<TargetAttribute>().ToArray();
+                        bool debug = Attribute.GetCustomAttribute(method, typeof(DebugAttribute)) != null;
+
+                        if (infixTargetAttribute == null)
+                            continue;
+
+                        MemberInfo? target = GetMember(infixTargetAttribute.type, infixTargetAttribute.memberName,
+                            infixTargetAttribute.parameterTypes, infixTargetAttribute.genericTypes);
+                        if (target == null)
+                            throw new InvalidOperationException("null wrapped member");
+
+                        foreach (var infixPatchAttribute in infixPatchAttributes)
+                        {
+                            var patchedType = infixPatchAttribute.type ?? harmonyAttribute.info.declaringType;
+
+                            MethodInfo? caller = (MethodInfo?)GetMember(patchedType, infixPatchAttribute.methodName,
+                                infixPatchAttribute.parameterTypes, infixPatchAttribute.genericTypes);
+                            if (caller == null)
+                                throw new InvalidOperationException("null target method");
+
+                            if (!StateBuilders.TryGetValue(caller, out StateBuilder<Type> stateBuilder))
+                                stateBuilder = StateBuilders[caller] = new();
+
+                            var arguments = method.GetParameters().Select(param => BindParameter(param, caller, target, stateBuilder))
+                                .ToArray();
+
+                            Patches.Add(new()
+                            {
+                                caller = caller,
+                                target = target,
+                                patchMethod = method,
+                                patchType = infixTargetAttribute.patchType,
+                                parameters = arguments,
+                                debug = debug,
+                            });
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw new InvalidOperationException($"Error processing {type}:{method}", e);
+                    }
+                }
+            }
+
+            PatchesByMethod = Patches.GroupBy(patch => patch.caller).ToDictionary(g => g.Key, g => g.ToList());
+        }
+
+        public MethodInfo CreatePatchTranspiler(
+            MethodInfo patchedMethod)
+        {
+            var patches = PatchesByMethod[patchedMethod];
+
+            MethodInfo? iteratorMethod = patchedMethod.GetIteratorImplementation();
+
+            if (iteratorMethod is not null)
+            {
+                if (patches.Any(p => p.HasBindingType(BindingType.State)))
+                    throw new NotSupportedException("__state is not supported for compiler-generated iterator methods");
+                if (patches.Any(p => p.HasBindingType(BindingType.Result)))
+                    throw new NotSupportedException("__result is not supported for compiler-generated iterator methods");
+
+                throw new NotImplementedException();
+            }
+
+            List<InstructionMatcher.Rule> rules = [];
+
+            if (!StateBuilders.TryGetValue(patchedMethod, out var stateBuilder))
+                stateBuilder = new();
+
+            if (stateBuilder.LocalTypes.Count > 0)
+                rules.Add(stateBuilder.BuildRule());
+
+            foreach (IGrouping<MemberInfo, PatchInfo> targetGroup in patches.GroupBy(patch => patch.target))
+            {
+                var target = targetGroup.Key;
+                var prefixes = targetGroup.Where(patch => patch.patchType == PatchType.InnerPrefix).ToList();
+                var postfixes = targetGroup.Where(patch => patch.patchType == PatchType.InnerPostfix).ToList();
+
+                rules.Add(new()
+                {
+                    LateGenerator = (_, _, generator) =>
+                        RedirectRule_Core(generator, patchedMethod, target, null, prefixes, postfixes, stateBuilder.LocalTypes),
+                });
+            }
+
+            var patchMatcher = new InstructionMatcher
+            {
+                Rules = rules,
+                LocalTypes = stateBuilder.LocalTypes,
+            };
+
+            int version = IncrementVersion(patchedMethod);
+            MethodInfo transpiler = MakeTranspiler([patchMatcher],
+                $"{patchedMethod.DeclaringType?.FullName?.Replace('.', '_')}_{patchedMethod.Name}_Transpiler{version}", false);
+            return transpiler;
+        }
+
+        public bool ShouldDebug(MethodInfo targetMethod) => PatchesByMethod[targetMethod].Any(p => p.debug);
+    }
+}
---
+++ Source_Disharmony/Autopatcher.RuleBuilder.cs
@@ -0,0 +1,301 @@
+﻿namespace Disharmony;
+
+public static partial class Autopatcher
+{
+    private class RuleBuilder
+    {
+        private readonly Type[] callerParameterTypes;
+        private readonly Type[] targetParameterTypes;
+        private readonly int[] parameterToLocalIndex;
+        private int resultLocalIndex = -1;
+        private readonly Type targetType;
+
+        private readonly ILGenerator generator;
+        private readonly MethodBase caller;
+        private readonly MemberInfo target;
+        private readonly MemberInfo? replacementTarget;
+        private readonly List<PatchInfo> prefixes;
+        private readonly List<PatchInfo> postfixes;
+        private readonly List<CodeInstruction> output = [];
+
+        private readonly List<Type> localTypes;
+
+        private readonly bool debug;
+
+        public RuleBuilder(
+            ILGenerator generator,
+            MethodBase caller,
+            MemberInfo target,
+            MethodInfo? replacementTarget,
+            List<PatchInfo> prefixes,
+            List<PatchInfo> postfixes,
+            List<Type> localTypes)
+        {
+            this.generator = generator;
+            this.caller = caller;
+            this.target = target;
+            this.replacementTarget = replacementTarget;
+            this.prefixes = prefixes;
+            this.postfixes = postfixes;
+            this.localTypes = localTypes.ToList();
+
+            debug = prefixes.Any(p => p.debug) || postfixes.Any(p => p.debug);
+
+            targetType = target switch
+            {
+                FieldInfo field => field.FieldType,
+                MethodInfo method => method.ReturnType,
+                _ => throw new NotSupportedException(),
+            };
+
+            callerParameterTypes = GetParameterTypes(caller);
+            targetParameterTypes = GetParameterTypes(target);
+
+            parameterToLocalIndex = new int[targetParameterTypes.Length];
+        }
+
+        private void EmitReplacement()
+        {
+            if (debug)
+                LogDebugInfo();
+
+            EmitPrelude();
+
+            var prefixesUsingResult = prefixes.Where(patch => patch.HasBindingType(BindingType.Result)).ToList();
+            var postfixesUsingResult = postfixes.Where(patch => patch.HasBindingType(BindingType.Result)).ToList();
+
+            if (prefixesUsingResult.Count > 0 || postfixesUsingResult.Count > 0)
+            {
+                resultLocalIndex = AddLocal(targetType);
+
+                if (prefixesUsingResult.Count > 0)
+                {
+                    if (!prefixesUsingResult[0].parameters.Single(a => a.BindingType == BindingType.Result).Parameter.IsOut)
+                        EmitInitializer(targetType, resultLocalIndex, output);
+                }
+            }
+
+            Label? skipLabel = null;
+            foreach (var prefix in prefixes)
+            {
+                MethodInfo patchMethod = prefix.patchMethod;
+                foreach (var parameter in prefix.parameters)
+                    EmitParameterValue(parameter);
+
+                output.Add(CodeInstruction.Annotation($"{prefix.patchType} {patchMethod.FullName}"));
+                output.Add(new(OpcodeFor(patchMethod), patchMethod));
+                if (!patchMethod.ReturnType.IsVoid())
+                {
+                    output.Add(new(OpCodes.Brfalse, skipLabel ??= generator.DefineLabel()));
+                }
+            }
+
+            for (int i = 0; i < targetParameterTypes.Length; i++)
+            {
+                EmitTargetParameter(targetParameterTypes[i], i);
+            }
+
+            if (replacementTarget != null)
+                output.Add(new(OpCodes.Call, replacementTarget));
+            else
+                output.Add(new(OpcodeFor(target), target));
+
+            if (skipLabel != null || postfixes.Count > 0)
+            {
+                if (resultLocalIndex >= 0)
+                    output.Add(CodeInstruction.StoreLocal(resultLocalIndex));
+
+                if (skipLabel is Label label)
+                {
+                    var branchTarget = new CodeInstruction(OpCodes.Nop);
+                    branchTarget.labels.Add(label);
+                    output.Add(branchTarget);
+                }
+
+                foreach (var postfix in postfixes)
+                {
+                    MethodInfo patchMethod = postfix.patchMethod;
+                    foreach (var parameter in postfix.parameters)
+                        EmitParameterValue(parameter);
+
+                    output.Add(CodeInstruction.Annotation($"{postfix.patchType} {patchMethod.FullName}"));
+                    output.Add(new(OpcodeFor(patchMethod), patchMethod));
+                    if (!patchMethod.ReturnType.IsVoid())
+                        output.Add(new(OpCodes.Pop));
+                }
+
+                if (resultLocalIndex >= 0)
+                {
+                    output.Add(CodeInstruction.LoadLocal(resultLocalIndex));
+                }
+            }
+        }
+
+        private void LogDebugInfo()
+        {
+            foreach (var patch in prefixes.Concat(postfixes))
+            {
+                FileLog.Log($"[{patch.patchType}] {patch.patchMethod.FullName}");
+                foreach (var parameter in patch.parameters)
+                {
+                    string fields = "";
+                    if (parameter.Fields is { Length: > 0 })
+                        fields = $" Fields=[{string.Join(", ", parameter.Fields.Select(f => f.Name))}]";
+                    FileLog.Log(
+                        $"Name={parameter.Parameter.Name} BindingType={parameter.BindingType} Scope={parameter.Scope} Index={parameter.Index}{fields}");
+                }
+            }
+        }
+
+        private void EmitParameterValue(ParameterBinding parameter)
+        {
+            Type parameterType = parameter.Parameter.ParameterType;
+
+            switch (parameter.BindingType)
+            {
+                case BindingType.Parameter:
+                case BindingType.Instance:
+                {
+                    EmitParameterLookup(parameterType);
+                    break;
+                }
+
+                case BindingType.Result:
+                {
+                    EmitResult(parameterType);
+                    break;
+                }
+
+                case BindingType.State:
+                {
+                    if (parameterType.IsByRef)
+                        output.Add(CodeInstruction.LoadLocalAddress(parameter.Index));
+                    else
+                        output.Add(CodeInstruction.LoadLocal(parameter.Index));
+
+                    break;
+                }
+
+                default:
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+            }
+
+            if (parameter.Fields is { Length: >0 })
+            {
+                foreach (var field in parameter.Fields)
+                {
+                    if (parameterType.IsByRef)
+                        output.Add(new(OpCodes.Ldflda, field));
+                    else
+                        output.Add(new(OpCodes.Ldfld, field));
+                }
+            }
+
+            void EmitParameterLookup(Type type)
+            {
+                switch (parameter.Scope)
+                {
+                    case Scope.Outer: EmitCallerParameter(type, parameter.Index); break;
+                    case Scope.Inner: EmitTargetParameter(type, parameter.Index); break;
+                    default: throw new ArgumentOutOfRangeException(nameof(parameter.Scope));
+                }
+            }
+        }
+
+        private void EmitResult(Type parameterType)
+        {
+            if (parameterType.IsByRef)
+            {
+                output.Add(CodeInstruction.LoadLocalAddress(resultLocalIndex));
+            }
+            else
+                output.Add(CodeInstruction.LoadLocal(resultLocalIndex));
+        }
+
+        private void EmitCallerParameter(Type type, int index)
+        {
+            if (type.IsByRef && !callerParameterTypes[index].IsByRef)
+                output.Add(new(OpCodes.Ldarga, index));
+            else
+                output.Add(CodeInstruction.LoadArgument(index));
+            if (!type.IsByRef && callerParameterTypes[index].IsByRef)
+                output.Add(new(OpCodes.Ldobj, type));
+        }
+
+        private void EmitTargetParameter(Type type, int index)
+        {
+            if (type.IsByRef && !targetParameterTypes[index].IsByRef)
+                output.Add(CodeInstruction.LoadLocalAddress(parameterToLocalIndex[index]));
+            else
+                output.Add(CodeInstruction.LoadLocal(parameterToLocalIndex[index]));
+            if (!type.IsByRef && targetParameterTypes[index].IsByRef)
+                output.Add(new(OpCodes.Ldobj, type));
+        }
+
+        private void EmitPrelude()
+        {
+            // Save all parameters to local. The matcher will handle renumbering the locals to new
+            // unused local indexes.
+            for (int i = targetParameterTypes.Length - 1; i >= 0; i--)
+            {
+                parameterToLocalIndex[i] = AddLocal(targetParameterTypes[i]);
+                output.Add(CodeInstruction.StoreLocal(parameterToLocalIndex[i]));
+            }
+        }
+
+        private int AddLocal(Type type)
+        {
+            var localIndex = localTypes.Count;
+            localTypes.Add(type);
+            return localIndex;
+        }
+
+        private static Type[] GetParameterTypes(MemberInfo member)
+        {
+            return member switch
+            {
+                FieldInfo { IsStatic: true } => [],
+                FieldInfo { IsStatic: false } field => [field.DeclaringType],
+                MethodInfo { IsStatic: true } method => [.. method.GetParameters().Select(p => p.ParameterType)],
+                MethodInfo { IsStatic: false } method => [method.DeclaringType, .. method.GetParameters().Select(p => p.ParameterType)],
+                _ => throw new InvalidOperationException(),
+            };
+        }
+
+        private static OpCode OpcodeFor(MemberInfo callee)
+        {
+            return callee switch
+            {
+                FieldInfo { IsStatic: true } => OpCodes.Ldsfld,
+                FieldInfo { IsStatic: false } => OpCodes.Ldfld,
+                MethodBase { IsVirtual: true } => OpCodes.Callvirt,
+                MethodBase { IsVirtual: false } => OpCodes.Call,
+                _ => throw new InvalidOperationException(),
+            };
+        }
+
+        public InstructionMatcher.Rule BuildRule()
+        {
+            List<CodeInstruction> pattern =
+            [
+                new(OpcodeFor(target), target),
+            ];
+
+            EmitReplacement();
+
+            return new InstructionMatcher.Rule
+            {
+                Min = 1,
+                Max = 0,
+                Mode = InstructionMatcher.OutputMode.Replace,
+                Pattern = pattern.ToArray(),
+                Output = output.ToArray(),
+                LocalTypes = localTypes.ToArray(),
+                Name = $"{target.DeclaringType?.FullName}::{target.Name}",
+            };
+        }
+    }
+}
