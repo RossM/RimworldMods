8549893013708cb4ab9ba2d51d4803803389049e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-17 03:59:03 -0700

    Use local function lookup

--- Source_Xylib/Patches/Patch_Building_GeneExtractor.cs
+++ Source_Xylib/Patches/Patch_Building_GeneExtractor.cs
@@ -6,31 +6,11 @@ namespace Xylib.Patches;
 internal static class Patch_Building_GeneExtractor
 {
     [Feature(nameof(DefModExtension_GeneWithComps.geneType))]
-    [HarmonyPostfix]
+    [Postfix]
+    [Target("Finish.SelectionWeight")]
     public static void SelectionWeight_Postfix(Gene g, ref float __result)
     {
         if (g.def.Extension_GeneWithComps?.geneType is GeneType.Endogene)
             __result = 0f;
-    }
-
-    [HarmonyTargetMethod]
-    public static MethodInfo? TargetMethod()
-    {
-        var type = AccessTools.TypeByName("RimWorld.Building_GeneExtractor");
-        if (type == null)
-            return null;
-        foreach (var nestedType in type.GetNestedTypes(AccessTools.all))
-        {
-            if (!nestedType.Name.StartsWith("<>c"))
-                continue;
-
-            foreach (var method in nestedType.GetMethods(AccessTools.all))
-            {
-                if (method.Name.StartsWith("<Finish>g__SelectionWeight"))
-                    return method;
-            }
-        }
-
-        return null;
     }
 }
