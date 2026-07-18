8bd01d4e5596a83d6a9f8214ec4b054dd730bf00
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-07-18 00:46:35 -0700

    Implement equality operators on Invocation subclasses

--- Source_Disharmony/Invocation.cs
+++ Source_Disharmony/Invocation.cs
@@ -8,26 +8,68 @@ internal abstract class Invocation
     public abstract Type[] GetParameterTypes();
 }
 
-internal class FieldInvocation(FieldInfo member) : Invocation
+internal class FieldInvocation(FieldInfo field) : Invocation
 {
-    public override string FullName => member.FullName;
+    private readonly FieldInfo field = field;
 
-    public override Type GetReturnType() => member.FieldType;
+    public override string FullName => @field.FullName;
 
-    public override CodeInstruction GetCodeInstruction() => new(member.IsStatic ? OpCodes.Ldsfld : OpCodes.Ldfld, member);
+    public override Type GetReturnType() => field.FieldType;
 
-    public override Type[] GetParameterTypes() => member.IsStatic ? [] : [member.DeclaringType];
+    public override CodeInstruction GetCodeInstruction() => new(field.IsStatic ? OpCodes.Ldsfld : OpCodes.Ldfld, field);
+
+    public override Type[] GetParameterTypes() => field.IsStatic ? [] : [field.DeclaringType];
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        if (obj.GetType() != GetType())
+            return false;
+        return Equals((FieldInvocation)obj);
+    }
+
+    protected bool Equals(FieldInvocation other) => field.Equals(other.field);
+
+    public override int GetHashCode() => field.GetHashCode();
+
+    public static bool operator ==(FieldInvocation? left, FieldInvocation? right) => Equals(left, right);
+
+    public static bool operator !=(FieldInvocation? left, FieldInvocation? right) => !Equals(left, right);
 }
 
-internal class MethodInvocation(MethodInfo member) : Invocation
+internal class MethodInvocation(MethodInfo method) : Invocation
 {
-    public override string FullName => member.FullName;
+    private readonly MethodInfo method = method;
 
-    public override Type GetReturnType() => member.ReturnType;
+    public override string FullName => method.FullName;
 
-    public override CodeInstruction GetCodeInstruction() => new(member.IsVirtual ? OpCodes.Callvirt : OpCodes.Call, member);
+    public override Type GetReturnType() => method.ReturnType;
 
-    public override Type[] GetParameterTypes() => member.IsStatic
-        ? [.. member.GetParameters().Select(p => p.ParameterType)]
-        : [member.DeclaringType, .. member.GetParameters().Select(p => p.ParameterType)];
+    public override CodeInstruction GetCodeInstruction() => new(method.IsVirtual ? OpCodes.Callvirt : OpCodes.Call, method);
+
+    public override Type[] GetParameterTypes() => method.IsStatic
+        ? [.. method.GetParameters().Select(p => p.ParameterType)]
+        : [method.DeclaringType, .. method.GetParameters().Select(p => p.ParameterType)];
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+            return false;
+        if (ReferenceEquals(this, obj))
+            return true;
+        if (obj.GetType() != GetType())
+            return false;
+        return Equals((MethodInvocation)obj);
+    }
+
+    protected bool Equals(MethodInvocation other) => method.Equals(other.method);
+
+    public override int GetHashCode() => method.GetHashCode();
+
+    public static bool operator ==(MethodInvocation? left, MethodInvocation? right) => Equals(left, right);
+
+    public static bool operator !=(MethodInvocation? left, MethodInvocation? right) => !Equals(left, right);
 }
