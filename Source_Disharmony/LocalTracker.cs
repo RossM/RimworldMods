875f02a9a1c3f055c5e9cb69089ca2b59fe6d442
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-08-10 06:51:34 -0700

    Add doc comments

--- Source_Disharmony/LocalTracker.cs
+++ Source_Disharmony/LocalTracker.cs
@@ -1,9 +1,18 @@
 ﻿namespace Disharmony;
 
+/// <summary>
+///     Abstract base class representing a local variable where we may or may not have a LocalBuilder and
+///     corresponding type information.
+/// </summary>
 internal abstract record LocalTracker
 {
     public abstract int Index { get; }
 
+    /// <summary>
+    ///     Gets an appropriate LocalTracker for the local referenced in an instruction.
+    /// </summary>
+    /// <param name="instruction"></param>
+    /// <returns></returns>
     public static LocalTracker From(CodeInstruction instruction)
     {
         return instruction.operand is LocalBuilder builder
@@ -11,6 +20,11 @@ internal abstract record LocalTracker
             : new LocalTrackerIndex(OpCodeData.GetIntOperand(instruction));
     }
 
+    /// <summary>
+    ///     Gives the same result as <c>LocalTracker.From(instruction).Index</c>, but without creating a LocalTracker object.
+    /// </summary>
+    /// <param name="instruction"></param>
+    /// <returns></returns>
     public static int IndexFrom(CodeInstruction instruction)
     {
         return instruction.operand is LocalBuilder builder
@@ -18,11 +32,24 @@ internal abstract record LocalTracker
             : OpCodeData.GetIntOperand(instruction);
     }
 
+    /// <summary>
+    ///     Gets a CodeInstruction that stores the top of the stack into this local variable.
+    /// </summary>
+    /// <returns></returns>
     public abstract CodeInstruction Store();
 
+    /// <summary>
+    ///     Gets a CodeInstruction that loads this local variable onto the stack, or its address if useAddress is true.
+    /// </summary>
+    /// <param name="useAddress"></param>
+    /// <returns></returns>
     public abstract CodeInstruction Load(bool useAddress = false);
 }
 
+/// <summary>
+///    Represents a local variable that has a LocalBuilder available.
+/// </summary>
+/// <param name="Builder"></param>
 internal record LocalTrackerBuilder(LocalBuilder Builder) : LocalTracker
 {
     public override int Index => Builder.LocalIndex;
@@ -36,6 +63,10 @@ internal record LocalTrackerBuilder(LocalBuilder Builder) : LocalTracker
         : new(Index <= byte.MaxValue ? OpCodes.Ldloc_S : OpCodes.Ldloc, Builder);
 }
 
+/// <summary>
+///     Represents a local variable by its index.
+/// </summary>
+/// <param name="Index"></param>
 internal record LocalTrackerIndex(int Index) : LocalTracker
 {
     public override int Index { get; } = Index;
