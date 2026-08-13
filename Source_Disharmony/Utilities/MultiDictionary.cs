952fd9ec0539fa483781217ceebea42eaf02bb9b
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-08-13 16:08:58 -0700

    Extend MultiDictionary API

--- Source_Disharmony/Utilities/MultiDictionary.cs
+++ Source_Disharmony/Utilities/MultiDictionary.cs
@@ -1,17 +1,29 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Disharmony.Utilities;
 
 /// <summary>
 ///     Associates each key with an insertion-ordered collection of values.
 /// </summary>
-internal sealed class MultiDictionary<TKey, TValue> where TKey : notnull
+internal sealed class MultiDictionary<TKey, TElement> : IEnumerable<IGrouping<TKey, TElement>> where TKey : notnull
 {
-    private readonly Dictionary<TKey, List<TValue>> valuesByKey = [];
-
-    public void Add(TKey key, TValue value)
+    private class Grouping(TKey key, IEnumerable<TElement> values) : IGrouping<TKey, TElement>
     {
-        if (!valuesByKey.TryGetValue(key, out List<TValue>? values))
+        public TKey Key { get; } = key;
+        public IEnumerator<TElement> GetEnumerator() => values.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)values).GetEnumerator();
+    }
+
+    private IEnumerable<IGrouping<TKey, TElement>> Groups => valuesByKey.Select(kvp => new Grouping(kvp.Key, kvp.Value));
+
+    private readonly Dictionary<TKey, List<TElement>> valuesByKey = [];
+
+    public IEnumerable<TElement> this[TKey key] => valuesByKey.TryGetValue(key, out var values) ? values : Array.Empty<TElement>();
+
+    public void Add(TKey key, TElement value)
+    {
+        if (!valuesByKey.TryGetValue(key, out List<TElement>? values))
         {
             values = [];
             valuesByKey.Add(key, values);
@@ -20,11 +32,24 @@ internal sealed class MultiDictionary<TKey, TValue> where TKey : notnull
         values.Add(value);
     }
 
+    public bool Remove(TKey key, TElement value) => valuesByKey.TryGetValue(key, out var values) && values.Remove(value);
+
+    public int RemoveAll(TKey key, Predicate<TElement> predicate) =>
+        valuesByKey.TryGetValue(key, out var values) ? values.RemoveAll(predicate) : 0;
+
+    public int RemoveAll(Func<TKey, TElement, bool> predicate)
+    {
+        int removed = 0;
+        foreach (var kvp in valuesByKey)
+            removed += kvp.Value.RemoveAll(value => predicate(kvp.Key, value));
+        return removed;
+    }
+
     public bool TryGetValues(
         TKey key,
-        [NotNullWhen(true)] out IReadOnlyList<TValue>? values)
+        [NotNullWhen(true)] out IReadOnlyList<TElement>? values)
     {
-        if (valuesByKey.TryGetValue(key, out List<TValue>? mutableValues))
+        if (valuesByKey.TryGetValue(key, out List<TElement>? mutableValues))
         {
             values = mutableValues;
             return true;
@@ -35,4 +60,7 @@ internal sealed class MultiDictionary<TKey, TValue> where TKey : notnull
     }
 
     public void Clear() => valuesByKey.Clear();
+
+    public IEnumerator<IGrouping<TKey, TElement>> GetEnumerator() => Groups.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Groups).GetEnumerator();
 }
