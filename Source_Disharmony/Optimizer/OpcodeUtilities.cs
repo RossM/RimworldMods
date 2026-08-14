49426447de9872db94bdbaa21ffe4c5a0a0debfc
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-08-13 22:30:19 -0700

    Add skeleton GetOutputType method

---
+++ Source_Disharmony/Optimizer/OpcodeUtilities.cs
@@ -0,0 +1,9 @@
+﻿namespace Disharmony.Optimizer;
+
+internal static class OpcodeUtilities
+{
+    public static Type GetOutputType(OpCode opcode, params Type[] inputTypes)
+    {
+        throw new NotImplementedException();
+    }
+}
