bfc538a72671932e48f4e70a00748d079371475d
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-08-14 00:19:05 -0700

    Add doc comment

--- Source_Disharmony/Optimizer/OpcodeUtilities.cs
+++ Source_Disharmony/Optimizer/OpcodeUtilities.cs
@@ -2,6 +2,18 @@
 
 internal static class OpcodeUtilities
 {
+    /// <summary>
+    ///     Gets the output type of a CIL operation, given its input types.
+    /// </summary>
+    /// <remarks>
+    ///     For operations with the <see cref="OpCodeFlags.Argument" /> or <see cref="OpCodeFlags.Local" /> flags,
+    ///     the type of the corresponding argument or local should be the first element of <paramref name="inputTypes"/>,
+    ///     with the types of values popped from the stack following it.
+    /// </remarks>
+    /// <param name="op"></param>
+    /// <param name="inputTypes"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
     public static Type GetOutputType(ILInstruction op, params Type[] inputTypes)
     {
         throw new NotImplementedException();
