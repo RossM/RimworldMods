fea97ba7e745b0a75c05b77a6957e1872215a9f6
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-08-29 13:41:42 -0700

    Code cleanup

--- Source_Disharmony/Utilities/Polyfills.cs
+++ Source_Disharmony/Utilities/Polyfills.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Disharmony.Utilities;
 
-namespace Disharmony.Utilities
+internal static class Polyfills
 {
-    internal static class Polyfills
+    extension<TKey, TValue>(KeyValuePair<TKey, TValue> kvp)
     {
-        extension<TKey, TValue>(KeyValuePair<TKey, TValue> kvp)
+        public void Deconstruct(out TKey key, out TValue value)
         {
-            public void Deconstruct(out TKey key, out TValue value)
-            {
-                key = kvp.Key;
-                value = kvp.Value;
-            }
+            key = kvp.Key;
+            value = kvp.Value;
         }
     }
 }
