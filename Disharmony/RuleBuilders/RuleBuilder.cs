f97a8457f0990f5606008465ae19213814ca2465
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-08 22:39:07 -0700

    Run resharper code cleanup

--- Disharmony/RuleBuilders/RuleBuilder.cs
+++ Disharmony/RuleBuilders/RuleBuilder.cs
@@ -115,6 +115,7 @@ internal abstract class RuleBuilder(RuleBuilderContext context, Invocation outer
                         output.Add(new(OpCodes.Call, InfoOf.FieldInfo_GetFieldFromHandle1));
                         break;
                 }
+
                 resultType = parameter.memberInfo!.GetType();
                 break;
             }
