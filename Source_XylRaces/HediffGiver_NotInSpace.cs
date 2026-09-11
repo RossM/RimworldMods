7cd6296db7021c9f4a32b2217fc1d56df7fb89e6
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-11 01:48:25 -0700

    Add planet sickness gene

---
+++ Source_XylRaces/HediffGiver_NotInSpace.cs
@@ -0,0 +1,12 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class HediffGiver_RandomNotInSpace : HediffGiver_Random
+{
+    public override float ChanceFactor(Pawn pawn)
+    {
+        if (pawn.Spawned && pawn.MapHeld.Tile.Valid && pawn.MapHeld.Tile.LayerDef.isSpace)
+            return 0f;
+        return base.ChanceFactor(pawn);
+    }
+}
