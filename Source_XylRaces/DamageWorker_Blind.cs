e2008b3de468091ab09eb34d7efd39f6aaf14ed4
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-11 00:50:07 -0700

    Add oil spray ability

---
+++ Source_XylRaces/DamageWorker_Blind.cs
@@ -0,0 +1,20 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class DamageWorker_Blind : DamageWorker
+{
+    public override DamageResult Apply(DamageInfo dinfo, Thing victim)
+    {
+        if (victim is Pawn pawn)
+        {
+            foreach (var part in pawn.health.hediffSet.GetNotMissingParts(tag: BodyPartTagDefOf.SightSource))
+            {
+                Hediff hediff = HediffMaker.MakeHediff(dinfo.Def.hediff, pawn);
+                hediff.Severity = dinfo.Amount;
+                pawn.health.AddHediff(hediff, part, dinfo);
+            }
+        }
+
+        return new DamageResult();
+    }
+}
