1db773ed12ed0fe7c079420d0cf1ec70e5f98a76
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-11 19:33:40 -0700

    Show time remaining for emergency reserves

---
+++ Source_XylRaces/HediffComp_SeverityPerDay_Stage.cs
@@ -0,0 +1,27 @@
+﻿namespace XylXenos;
+
+[UsedFromXml]
+public class HediffComp_SeverityPerDay_Stage : HediffComp_SeverityPerDay
+{
+    private HediffCompProperties_SeverityPerDay Props => (HediffCompProperties_SeverityPerDay)props;
+
+    public override string? CompLabelInBracketsExtra
+    {
+        get
+        {
+            if (Props.showHoursToRecover && SeverityChangePerDay() < 0f)
+                return Mathf.RoundToInt((parent.Severity - parent.CurStage.minSeverity) / Mathf.Abs(SeverityChangePerDay()) * 24f).ToString() + "LetterHour".Translate();
+            return null;
+        }
+    }
+
+    public override string? CompTipStringExtra
+    {
+        get
+        {
+            if (Props.showDaysToRecover && SeverityChangePerDay() < 0f)
+                return "DaysToRecover".Translate(((parent.Severity - parent.CurStage.minSeverity) / Mathf.Abs(SeverityChangePerDay())).ToString("0.0")).Resolve();
+            return null;
+        }
+    }
+}
