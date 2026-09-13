2f88f071eca6a49942dd0c73fddb204bc016c7b8
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-13 05:22:24 -0700

    Revise HediffComp_SetNeed

--- Source_Xylib/HediffComp_SetNeed.cs
+++ Source_Xylib/HediffComp_SetNeed.cs
@@ -5,8 +5,8 @@
 public class HediffCompProperties_SetNeed : HediffCompProperties
 {
     public required NeedDef need;
-    public FloatRange levelPercentage;
-    public FloatRange severityRange = new(float.MinValue, float.MaxValue);
+    public FloatRange range;
+    public IntRange stages = new IntRange(int.MinValue, int.MaxValue);
 
     public HediffCompProperties_SetNeed()
     {
@@ -19,7 +19,7 @@ public class HediffComp_SetNeed : HediffComp
 {
     public HediffCompProperties_SetNeed Props => (HediffCompProperties_SetNeed)props;
 
-    private bool Active => Need != null && Props.severityRange.Includes(parent.Severity);
+    private bool Active => Need != null && Props.stages.Includes(parent.CurStageIndex);
 
     public Need? Need
     {
@@ -38,20 +38,20 @@ public class HediffComp_SetNeed : HediffComp
             if (!Active)
                 return null;
 
-            if (Props.levelPercentage.min <= 0)
-                return $"  - {Props.need.LabelCap}: {"max".Translate().CapitalizeFirst()} {Props.levelPercentage.max.ToStringPercent()}";
-            if (Props.levelPercentage.max >= 1)
-                return $"  - {Props.need.LabelCap}: {"min".Translate().CapitalizeFirst()} {Props.levelPercentage.min.ToStringPercent()}";
-            if (Props.levelPercentage.min == Props.levelPercentage.max)
-                return $"  - {Props.need.LabelCap}: {Props.levelPercentage.min.ToStringPercent()}";
+            if (Props.range.min <= 0)
+                return $"  - {Props.need.LabelCap}: {"max".Translate().CapitalizeFirst()} {Props.range.max.ToStringPercent()}";
+            if (Props.range.max >= 1)
+                return $"  - {Props.need.LabelCap}: {"min".Translate().CapitalizeFirst()} {Props.range.min.ToStringPercent()}";
+            if (Props.range.min == Props.range.max)
+                return $"  - {Props.need.LabelCap}: {Props.range.min.ToStringPercent()}";
             return
-                $"  - {Props.need.LabelCap}: {Props.levelPercentage.min.ToStringPercent()}-{Props.levelPercentage.max.ToStringPercent()}";
+                $"  - {Props.need.LabelCap}: {Props.range.min.ToStringPercent()}-{Props.range.max.ToStringPercent()}";
         }
     }
 
     public override void CompPostTickInterval(ref float severityAdjustment, int delta)
     {
         if (Active)
-            Need?.CurLevelPercentage = Mathf.Clamp(Need.CurLevelPercentage, Props.levelPercentage.min, Props.levelPercentage.max);
+            Need?.CurLevelPercentage = Mathf.Clamp(Need.CurLevelPercentage, Props.range.min, Props.range.max);
     }
 }
