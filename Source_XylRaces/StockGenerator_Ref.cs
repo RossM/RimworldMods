158c8d4233c34ea73c6da5aceb7918fa44781a0e
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-17 22:10:54 -0700

    Reference base stock generators rather than copy/paste

---
+++ Source_XylRaces/StockGenerator_Ref.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld.Planet;
+
+namespace XylXenos;
+
+[UsedFromXml]
+public class StockGenerator_Ref : StockGenerator
+{
+    public required TraderKindDef traderKind;
+
+    public override IEnumerable<Thing> GenerateThings(PlanetTile forTile, Faction? faction = null)
+    {
+        foreach (var stockGenerator in traderKind.stockGenerators)
+        foreach (var thing in stockGenerator.GenerateThings(forTile, faction))
+            yield return thing;
+    }
+
+    public override bool HandlesThingDef(ThingDef thingDef)
+    {
+        return traderKind.stockGenerators.Any(s => s.HandlesThingDef(thingDef));
+    }
+}
