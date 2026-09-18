96fcf120521effd3a73a5059e35fdbcf75ca37be
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-18 09:11:03 -0700

    Use nameof rather than string

--- Source_XylRaces/Patches/Patch_JobDriver_PlantWork.cs
+++ Source_XylRaces/Patches/Patch_JobDriver_PlantWork.cs
@@ -3,7 +3,7 @@
 [Patch(typeof(JobDriver_PlantWork))]
 public static class Patch_JobDriver_PlantWork
 {
-    [Feature("XylTreeCuttingSpeed")]
+    [Feature(nameof(DefOf.XylTreeCuttingSpeed))]
     [Postfix]
     [Target(nameof(JobDriver_PlantWork.WorkDonePerTick))]
     public static void WorkDonePerTick_Postfix(Pawn actor, Plant plant, ref float __result)
