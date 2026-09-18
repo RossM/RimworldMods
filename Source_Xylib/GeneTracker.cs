6fd9d86c1a633d4f760716382088c3c9000b086f
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-18 04:20:03 -0700

    Rename parameters

--- Source_Xylib/GeneTracker.cs
+++ Source_Xylib/GeneTracker.cs
@@ -32,7 +32,7 @@ public abstract class GeneTracker : IEventListener, IPawnData
             dest.AddRange(source);
     }
 
-    protected static void Multiply<TItem, TKey>(ref Dictionary<TKey, float>? dest, List<TItem>? source, Func<TItem, TKey> keyExtractor, Func<TItem, float> valueExtractor)
+    protected static void Multiply<TItem, TKey>(ref Dictionary<TKey, float>? dest, List<TItem>? source, Func<TItem, TKey> keySelector, Func<TItem, float> valueSelector)
     {
         if (source is null)
             return;
@@ -40,8 +40,8 @@ public abstract class GeneTracker : IEventListener, IPawnData
         foreach (var item in source)
         {
             dest ??= [];
-            var key = keyExtractor(item);
-            var value = valueExtractor(item);
+            var key = keySelector(item);
+            var value = valueSelector(item);
             dest[key] = dest.GetValueOrDefault(key, 1f) * value;
         }
     }
