41c01d8468970ecf653008a14652d8c7b21fafe1
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-19 23:14:57 -0700

    Fix beaver nose profile rendering

---
+++ Source_XylRaces/PawnRenderNodeWorker_HeadAttachment.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace XylXenos;
+
+[UsedFromXml]
+public class PawnRenderNodeWorker_HeadAttachment : PawnRenderNodeWorker_FlipWhenCrawling
+{
+    public override Vector3 OffsetFor(PawnRenderNode node, PawnDrawParms parms, [UnscopedRef] out Vector3 pivot)
+    {
+        var result = base.OffsetFor(node, parms, out pivot);
+
+        if (parms.pawn.story.headType.narrow && node.Props.narrowCrownHorizontalOffset != 0f && parms.facing.IsHorizontal)
+        {
+            if (parms.facing == Rot4.East)
+                result.x -= node.Props.narrowCrownHorizontalOffset;
+            else if (parms.facing == Rot4.West)
+            {
+                result.x += node.Props.narrowCrownHorizontalOffset;
+            }
+            result.z -= node.Props.narrowCrownHorizontalOffset;
+        }
+
+        return result;
+    }
+}
