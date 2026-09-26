df0f9be03fed529f8f00e1c8c518d54feedd5c76
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2026-09-26 13:50:40 -0700

    Warn before installing a part which will cause bio-rejection

--- Source_XylRaces/DefOf.cs
+++ Source_XylRaces/DefOf.cs
@@ -10,6 +10,8 @@ public static class DefOf
 
     public static GeneCategoryDef Cosmetic_Skin;
 
+    public static GeneDef XylBioRejection;
+
     public static GeneDef XylDocile;
 
     public static GeneDef XylEcholocation;
---
+++ Source_XylRaces/Patches/Patch_RecipeWorker.cs
@@ -0,0 +1,19 @@
+﻿namespace XylXenos.Patches;
+
+[Patch(typeof(RecipeWorker))]
+public static class Patch_RecipeWorker
+{
+    [Feature(nameof(DefOf.XylBioRejection))]
+    [Postfix]
+    [Target(nameof(RecipeWorker.GetLabelWhenUsedOn))]
+    public static void RecipeWorker_Postfix(RecipeWorker __instance, Pawn pawn, BodyPartRecord part, ref string __result)
+    {
+        if (__instance is not (Recipe_InstallArtificialBodyPart or Recipe_InstallImplant))
+            return;
+        if (!pawn.HasActiveGene(DefOf.XylBioRejection))
+            return;
+        if (!__instance.recipe.addsHediff.countsAsAddedPartOrImplant)
+            return;
+        __result += $" ({"XylCausesBioRejection".Translate()})";
+    }
+}
